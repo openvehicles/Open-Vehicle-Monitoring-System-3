1b84a004cde367bd4f3ca2457ad22b36ea839793
Mark Webb-Johnson <mark@webb-johnson.net>
2017-09-15 13:11:59 +0800

    Mongoose and Duktape as optional components

--- vehicle/OVMS.V3/components/mongoose/component.mk
+++ vehicle/OVMS.V3/components/mongoose/component.mk
@@ -7,11 +7,11 @@
 # please read the ESP-IDF documents if you need to do this.
 #
 
-#ifdef CONFIG_OVMS_SC_GPL_MONGOOSE
+ifdef CONFIG_OVMS_SC_GPL_MONGOOSE
 COMPONENT_ADD_INCLUDEDIRS:=.
 COMPONENT_SRCDIRS:=.
 #COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
-#else
+else
 COMPONENT_ADD_INCLUDEDIRS:=stub
 COMPONENT_SRCDIRS:=stub
-#endif //#ifdef CONFIG_OVMS_SC_GPL_MONGOOSE
+endif
