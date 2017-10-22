32ee4195118c8a18715bdaab91fbe5baa49fdbc8
Stephen Casner <casner@acm.org>
2017-10-22 00:37:19 -0700

    Track heap allocations per region; move static storage to IRAM heap
    - "module memory" command now shows how much free space and total
      space is available for 8-bit-accessible allocations (e.g., malloc)
      and for 32-bit accessible allocations.  For each task, the total
      allocation and changes are shown separately for DRAM, D/IRAM and
      IRAM regions.
    
    - Instead of allocating the storage arrays for memory debugging in
      static memory these arrays are now dynamically allocated from the
      IRAM (32-bit-accessible) heap that was otherwise unused.  This
      required some code changes to avoid 8-bit accesses.

--- vehicle/OVMS.V3/main/ovms_module.cpp
+++ vehicle/OVMS.V3/main/ovms_module.cpp
@@ -40,15 +40,38 @@ static const char *TAG = "ovms-module";
 #include "ovms_command.h"
 
 #ifdef configENABLE_MEMORY_DEBUG_DUMP
-#define DUMPSIZE 400
-#define TOTALSIZE 32
+#define DUMPSIZE 1000
+#define NUMTASKS 32
+#define NUMTAGS 3
 
-typedef struct
+class FreeHeap
   {
-  char task[4];
-  int before;
-  int after;
-  } HeapTask;
+  public:
+    inline FreeHeap()
+      {
+      m_free_8bit = xPortGetFreeHeapSizeCaps(MALLOC_CAP_8BIT);
+      m_free_32bit = xPortGetFreeHeapSizeCaps(MALLOC_CAP_32BIT) - m_free_8bit;
+      }
+    size_t Free8bit() { return m_free_8bit; }
+    size_t Free32bit() { return m_free_32bit; }
+
+  private:
+    size_t m_free_8bit;
+    size_t m_free_32bit;
+  } initial  __attribute__ ((init_priority (0150)));
+
+class HeapTask
+  {
+  public:
+    HeapTask()
+      {
+      for (int i = 0; i < NUMTAGS; ++i)
+        before[i] = after[i] = 0;
+      }
+    char task[4];
+    int before[NUMTAGS];
+    int after[NUMTAGS];
+  };
 
 class HeapTotals
   {
@@ -59,29 +82,30 @@ class HeapTotals
     HeapTask& operator[](size_t index) { return tasks[index]; };
     void clear() { count = 0; }
     int find(char* task)
-    {
-    for (int i = 0; i < count; ++i)
-      if (strcmp(task, tasks[i].task) == 0)
-        return i;
-    return -1;
-    }
-    void append(HeapTask& t)
-    {
-    if (count < TOTALSIZE)
       {
-      tasks[count] = t;
-      ++count;
+      for (int i = 0; i < count; ++i)
+        if (*(int*)task == *(int*)(tasks[i].task))
+          return i;
+      return -1;
       }
-    }
+    void append(HeapTask& t)
+      {
+      if (count < NUMTASKS)
+        {
+        tasks[count] = t;
+        ++count;
+        }
+      }
     
   private:
-    HeapTask tasks[TOTALSIZE];
+    HeapTask tasks[NUMTASKS];
     int count;
   };
 
-static mem_dump_block_t before[DUMPSIZE], after[DUMPSIZE];
+static mem_dump_block_t* before = NULL;
+static mem_dump_block_t* after = NULL;
 static size_t numbefore = 0, numafter = 0;
-static HeapTotals changes;
+static HeapTotals* changes = NULL;
 
 static void print_blocks(OvmsWriter* writer, const char* task)
   {
@@ -89,7 +113,7 @@ static void print_blocks(OvmsWriter* writer, const char* task)
   bool separate = false;
   for (int i = 0; i < numbefore; ++i)
     {
-    if (strcmp(before[i].task, task) != 0)
+    if (*(int*)(before[i].task) != *(int*)task)
       continue;
     int j = 0;
     for ( ; j < numafter; ++j)
@@ -97,7 +121,9 @@ static void print_blocks(OvmsWriter* writer, const char* task)
         break;
     if (j == numafter)
       {
-      writer->printf("- t=%s s=%4d a=%p\n", before[i].task, before[i].size, before[i].address);
+      char task[4];
+      *(int*)task = *(int*)(before[i].task);
+      writer->printf("- t=%s s=%4d a=%p\n", task, before[i].size, before[i].address);
       ++count;
       }
     }
@@ -107,7 +133,7 @@ static void print_blocks(OvmsWriter* writer, const char* task)
   count = 0;
   for (int i = 0; i < numafter; ++i)
     {
-    if (strcmp(after[i].task, task) != 0)
+    if (*(int*)(after[i].task) != *(int*)task)
       continue;
     int j = 0;
     for ( ; j < numbefore; ++j)
@@ -121,8 +147,10 @@ static void print_blocks(OvmsWriter* writer, const char* task)
         writer->printf("----------------------------\n");
         separate = false;
         }
+      char task[4];
+      *(int*)task = *(int*)(after[i].task);
       writer->printf("  t=%s s=%4d a=%p  %08X %08X %08X %08X %08X %08X %08X %08X\n",
-        after[i].task, after[i].size, p, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]);
+        task, after[i].size, p, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]);
       ++count;
       }
     }
@@ -131,7 +159,7 @@ static void print_blocks(OvmsWriter* writer, const char* task)
   total += count;
   for (int i = 0; i < numafter; ++i)
     {
-    if (strcmp(after[i].task, task) != 0)
+    if (*(int*)(after[i].task) != *(int*)task)
       continue;
     int j = 0;
     for ( ; j < numbefore; ++j)
@@ -145,8 +173,10 @@ static void print_blocks(OvmsWriter* writer, const char* task)
         writer->printf("----------------------------\n");
         separate = false;
         }
+      char task[4];
+      *(int*)task = *(int*)(after[i].task);
       writer->printf("+ t=%s s=%4d a=%p  %08X %08X %08X %08X %08X %08X %08X %08X\n",
-        after[i].task, after[i].size, p, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]);
+        task, after[i].size, p, p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]);
       ++total;
       }
     }
@@ -167,51 +197,75 @@ void module_memory(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc
     else
       tasks = argv;
     }
+  if (!before)
+    {
+    before = (mem_dump_block_t*)pvPortMallocCaps(sizeof(mem_dump_block_t)*DUMPSIZE, MALLOC_CAP_32BIT);
+    after = (mem_dump_block_t*)pvPortMallocCaps(sizeof(mem_dump_block_t)*DUMPSIZE, MALLOC_CAP_32BIT);
+    changes = (HeapTotals*)pvPortMallocCaps(sizeof(HeapTotals), MALLOC_CAP_32BIT);
+    }
 
   numafter = mem_debug_malloc_dump(0, after, DUMPSIZE);
 
-  changes.clear();
+  changes->clear();
   for (int i = 0; i < numbefore; ++i)
     {
-    int index = changes.find(before[i].task);
+    int tag = before[i].xtag;
+    if (tag >= NUMTAGS)
+      continue;
+    int index = changes->find(before[i].task);
     if (index >= 0)
       {
-      changes[index].before += before[i].size;
+      (*changes)[index].before[tag] += before[i].size;
       }
     else
       {
       HeapTask task;
       *(int*)task.task = *(int*)before[i].task;
-      task.before = before[i].size;
-      task.after = 0;
-      changes.append(task);
+      task.before[tag] = before[i].size;
+      changes->append(task);
       }
     }
   for (int i = 0; i < numafter; ++i)
     {
-    int index = changes.find(after[i].task);
+    int tag = after[i].xtag;
+    if (tag >= NUMTAGS)
+      continue;
+    int index = changes->find(after[i].task);
     if (index >= 0)
       {
-      changes[index].after += after[i].size;
+      (*changes)[index].after[tag] += after[i].size;
       }
     else
       {
       HeapTask task;
       *(int*)task.task = *(int*)after[i].task;
-      task.before = 0;
-      task.after = after[i].size;
-      changes.append(task);
+      task.after[tag] = after[i].size;
+      changes->append(task);
       }
     }
 
   writer->printf("============================\n");
-  uint32_t caps = MALLOC_CAP_8BIT;
-  writer->printf("Free %zu,  numafter = %d\n", xPortGetFreeHeapSizeCaps(caps), numafter);
-  for (int i = changes.begin(); i < changes.end(); ++i)
+  FreeHeap now;
+  writer->printf("Free 8-bit %zu/%zu, 32-bit %zu/%zu, numafter = %d\n",
+    now.Free8bit(), initial.Free8bit(), now.Free32bit(), initial.Free32bit(), numafter);
+  for (int i = changes->begin(); i < changes->end(); ++i)
     {
-    int change = changes[i].after - changes[i].before;
-    if (change)
-      writer->printf("task=%s total=%7d change=%+d\n", changes[i].task, changes[i].after, change);
+    int change[NUMTAGS];
+    bool any = false;
+    for (int j = 0; j < NUMTAGS; ++j)
+      {
+      change[j] = (*changes)[i].after[j] - (*changes)[i].before[j];
+      if (change[j])
+        any = true;
+      }
+    if (any)
+      {
+      char task[4];
+      *(int*)task = *(int*)((*changes)[i].task);
+      writer->printf("task=%s total=%7d%7d%7d change=%+7d%+7d%+7d\n", task,
+        (*changes)[i].after[0], (*changes)[i].after[1], (*changes)[i].after[2],
+        change[0], change[1], change[2]);
+      }
     }
 
   writer->printf("============================\n");
@@ -225,11 +279,16 @@ void module_memory(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc
     }
   else
     {
-    for (int i = changes.begin(); i < changes.end(); ++i)
+    for (int i = changes->begin(); i < changes->end(); ++i)
       {
-      int change = changes[i].after - changes[i].before;
-      if (change)
-        print_blocks(writer, changes[i].task);
+      bool any = false;
+      for (int j = 0; j < NUMTAGS; ++j)
+        {
+        if ((*changes)[i].after[j] - (*changes)[i].before[j] != 0)
+          any = true;
+        }
+      if (any)
+        print_blocks(writer, (*changes)[i].task);
       }
     }
 
@@ -302,25 +361,19 @@ void module_abort(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc,
 #endif
   }
 
-void module_task(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc, const char* const* argv)
-  {
-  }
-
 class OvmsModuleInit
   {
-  public: OvmsModuleInit();
-} MyOvmsModuleInit  __attribute__ ((init_priority (5100)));
+  public:
+  OvmsModuleInit()
+    {
+    ESP_LOGI(TAG, "Initialising MODULE (5100)");
 
-OvmsModuleInit::OvmsModuleInit()
-  {
-  ESP_LOGI(TAG, "Initialising MODULE (5100)");
-
-  OvmsCommand* cmd_module = MyCommandApp.RegisterCommand("module","Module framework",NULL);
-  cmd_module->RegisterCommand("memory","Show module memory usage",module_memory,"",0,0,true);
-  cmd_module->RegisterCommand("tasks","Show module task usage",module_tasks,"",0,0,true);
-  //cmd_module->RegisterCommand("task","Show module task detail",module_task,"<taskid>",1,1,true);
-  cmd_module->RegisterCommand("abort","Set trap to abort on malloc",module_abort,"<task> <count> [<size>]",2,3,true);
-  }
+    OvmsCommand* cmd_module = MyCommandApp.RegisterCommand("module","Module framework",NULL);
+    cmd_module->RegisterCommand("memory","Show module memory usage",module_memory,"",0,0,true);
+    cmd_module->RegisterCommand("tasks","Show module task usage",module_tasks,"",0,0,true);
+    cmd_module->RegisterCommand("abort","Set trap to abort on malloc",module_abort,"<task> <count> [<size>]",2,3,true);
+    }
+  } MyOvmsModuleInit  __attribute__ ((init_priority (5100)));
 
 // Returns the value of the stack pointer in the calling function.
 // The stack frame for this function is 32 bytes, hence the add.
