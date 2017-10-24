dfdd08b2ee547d7089f992bb72971a669f005a44
Mark Webb-Johnson <mark@webb-johnson.net>
2017-10-24 21:59:06 +0800

    First run at a Reverse Engineering Tools framework

---
+++ vehicle/OVMS.V3/components/retools/src/retools.h
@@ -0,0 +1,78 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          14th March 2017
+;
+;    Changes:
+;    1.0  Initial release
+;
+;    (C) 2011       Michael Stegen / Stegen Electronics
+;    (C) 2011-2017  Mark Webb-Johnson
+;    (C) 2011        Sonny Chen @ EPRO/DX
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#ifndef __RETOOLS_H__
+#define __RETOOLS_H__
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include <string>
+#include <map>
+#include "can.h"
+#include "pcp.h"
+
+typedef struct
+  {
+  CAN_frame_t last;
+  uint32_t rxcount;
+  } re_record_t;
+
+typedef std::map<uint32_t, uint8_t> re_id_map_t;
+typedef std::map<std::string, re_record_t*> re_record_map_t;
+
+class re : public pcp
+  {
+  public:
+    re(const char* name);
+    ~re();
+
+  public:
+    virtual void SetPowerMode(PowerMode powermode);
+
+  public:
+    void Task();
+    void Lock();
+    void Unlock();
+    void Clear();
+    std::string GetKey(CAN_frame_t* frame);
+
+  protected:
+    TaskHandle_t m_task;
+    QueueHandle_t m_rxqueue;
+
+  public:
+    QueueHandle_t m_mutex;
+    re_id_map_t m_idmap;
+    re_record_map_t m_rmap;
+    uint32_t m_started;
+  };
+
+#endif //#ifndef __RETOOLS_H__
