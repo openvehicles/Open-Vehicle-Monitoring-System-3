04a1e9c83e79e8bbdde878b8c8bf1b5d87a5dda3
Michael Balzer <balzer@expeedo.de>
2017-12-24 10:13:13 +0100

    Twizy: extension namespace harmonized to "xrt" (metrics & config)

--- vehicle/OVMS.V3/components/vehicle_renaulttwizy/src/vehicle_renaulttwizy.cpp
+++ vehicle/OVMS.V3/components/vehicle_renaulttwizy/src/vehicle_renaulttwizy.cpp
@@ -60,7 +60,7 @@ OvmsVehicleRenaultTwizy::OvmsVehicleRenaultTwizy()
   RegisterCanBus(1, CAN_MODE_ACTIVE, CAN_SPEED_500KBPS);
   
   // init configs:
-  MyConfig.RegisterParam("x.rt", "Renault Twizy", true, true);
+  MyConfig.RegisterParam("xrt", "Renault Twizy", true, true);
   ConfigChanged(NULL);
   
   // init metrics:
@@ -68,7 +68,7 @@ OvmsVehicleRenaultTwizy::OvmsVehicleRenaultTwizy()
     m_modifier = MyMetrics.RegisterModifier();
     ESP_LOGD(TAG, "registered metric modifier is #%d", m_modifier);
   }
-  m_version = MyMetrics.InitString("x.rt.m.version", 0, VERSION " " __DATE__ " " __TIME__);
+  m_version = MyMetrics.InitString("xrt.m.version", 0, VERSION " " __DATE__ " " __TIME__);
   
   // init commands:
   cmd_xrt = MyCommandApp.RegisterCommand("xrt", "Renault Twizy", NULL, "", 0, 0, true);
@@ -132,32 +132,32 @@ void OvmsVehicleRenaultTwizy::ConfigChanged(OvmsConfigParam* param)
   //  kd_compzero       Kickdown pedal compensation (Default: 120)
   //  
   
-  cfg_maxrange = MyConfig.GetParamValueInt("x.rt", "maxrange", CFG_DEFAULT_MAXRANGE);
+  cfg_maxrange = MyConfig.GetParamValueInt("xrt", "maxrange", CFG_DEFAULT_MAXRANGE);
   if (cfg_maxrange <= 0)
     cfg_maxrange = CFG_DEFAULT_MAXRANGE;
   
-  cfg_suffsoc = MyConfig.GetParamValueInt("x.rt", "suffsoc");
+  cfg_suffsoc = MyConfig.GetParamValueInt("xrt", "suffsoc");
   *StdMetrics.ms_v_charge_limit_soc = (float) cfg_suffsoc;
   
-  cfg_suffrange = MyConfig.GetParamValueInt("x.rt", "suffrange");
+  cfg_suffrange = MyConfig.GetParamValueInt("xrt", "suffrange");
   *StdMetrics.ms_v_charge_limit_range = (float) cfg_suffrange;
   
-  cfg_chargemode = MyConfig.GetParamValueInt("x.rt", "chargemode");
-  cfg_chargelevel = MyConfig.GetParamValueInt("x.rt", "chargelevel");
+  cfg_chargemode = MyConfig.GetParamValueInt("xrt", "chargemode");
+  cfg_chargelevel = MyConfig.GetParamValueInt("xrt", "chargelevel");
   
-  cfg_bat_cap_actual_prc = MyConfig.GetParamValueFloat("x.rt", "cap_act_prc", 100);
-  cfg_bat_cap_nominal_ah = MyConfig.GetParamValueFloat("x.rt", "cap_nom_ah", CFG_DEFAULT_CAPACITY);
+  cfg_bat_cap_actual_prc = MyConfig.GetParamValueFloat("xrt", "cap_act_prc", 100);
+  cfg_bat_cap_nominal_ah = MyConfig.GetParamValueFloat("xrt", "cap_nom_ah", CFG_DEFAULT_CAPACITY);
   
-  twizy_flags.EnableWrite = MyConfig.GetParamValueBool("x.rt", "canwrite", false);
-  twizy_flags.DisableReset = !MyConfig.GetParamValueBool("x.rt", "autoreset", true);
-  twizy_flags.DisableKickdown = !MyConfig.GetParamValueBool("x.rt", "kickdown", true);
-  twizy_flags.DisableAutoPower = !MyConfig.GetParamValueBool("x.rt", "autopower", true);
-  twizy_flags.EnableInputs = MyConfig.GetParamValueBool("x.rt", "console", false);
+  twizy_flags.EnableWrite = MyConfig.GetParamValueBool("xrt", "canwrite", false);
+  twizy_flags.DisableReset = !MyConfig.GetParamValueBool("xrt", "autoreset", true);
+  twizy_flags.DisableKickdown = !MyConfig.GetParamValueBool("xrt", "kickdown", true);
+  twizy_flags.DisableAutoPower = !MyConfig.GetParamValueBool("xrt", "autopower", true);
+  twizy_flags.EnableInputs = MyConfig.GetParamValueBool("xrt", "console", false);
   
-  cfg_gpslog_interval = MyConfig.GetParamValueInt("x.rt", "gpslogint", 0);
+  cfg_gpslog_interval = MyConfig.GetParamValueInt("xrt", "gpslogint", 0);
 
-  cfg_kd_threshold = MyConfig.GetParamValueInt("x.rt", "kd_threshold", CFG_DEFAULT_KD_THRESHOLD);
-  cfg_kd_compzero = MyConfig.GetParamValueInt("x.rt", "kd_compzero", CFG_DEFAULT_KD_COMPZERO);
+  cfg_kd_threshold = MyConfig.GetParamValueInt("xrt", "kd_threshold", CFG_DEFAULT_KD_THRESHOLD);
+  cfg_kd_compzero = MyConfig.GetParamValueInt("xrt", "kd_compzero", CFG_DEFAULT_KD_COMPZERO);
 }
 
 
@@ -170,44 +170,44 @@ bool OvmsVehicleRenaultTwizy::SetFeature(int key, const char *value)
   switch (key)
   {
     case 0:
-      MyConfig.SetParamValue("x.rt", "gpslogint", value);
+      MyConfig.SetParamValue("xrt", "gpslogint", value);
       return true;
     case 1:
-      MyConfig.SetParamValue("x.rt", "kd_threshold", value);
+      MyConfig.SetParamValue("xrt", "kd_threshold", value);
       return true;
     case 2:
-      MyConfig.SetParamValue("x.rt", "kd_compzero", value);
+      MyConfig.SetParamValue("xrt", "kd_compzero", value);
       return true;
     case 6:
-      MyConfig.SetParamValue("x.rt", "chargemode", value);
+      MyConfig.SetParamValue("xrt", "chargemode", value);
       return true;
     case 7:
-      MyConfig.SetParamValue("x.rt", "chargelevel", value);
+      MyConfig.SetParamValue("xrt", "chargelevel", value);
       return true;
     case 10:
-      MyConfig.SetParamValue("x.rt", "suffsoc", value);
+      MyConfig.SetParamValue("xrt", "suffsoc", value);
       return true;
     case 11:
-      MyConfig.SetParamValue("x.rt", "suffrange", value);
+      MyConfig.SetParamValue("xrt", "suffrange", value);
       return true;
     case 12:
-      MyConfig.SetParamValue("x.rt", "maxrange", value);
+      MyConfig.SetParamValue("xrt", "maxrange", value);
       return true;
     case 13:
-      MyConfig.SetParamValue("x.rt", "cap_act_prc", value);
+      MyConfig.SetParamValue("xrt", "cap_act_prc", value);
       return true;
     case 15:
     {
       int bits = atoi(value);
-      MyConfig.SetParamValueBool("x.rt", "canwrite",  (bits& 1)!=0);
-      MyConfig.SetParamValueBool("x.rt", "autoreset", (bits& 2)==0);
-      MyConfig.SetParamValueBool("x.rt", "kickdown",  (bits& 4)==0);
-      MyConfig.SetParamValueBool("x.rt", "autopower", (bits& 8)==0);
-      MyConfig.SetParamValueBool("x.rt", "console",   (bits&16)!=0);
+      MyConfig.SetParamValueBool("xrt", "canwrite",  (bits& 1)!=0);
+      MyConfig.SetParamValueBool("xrt", "autoreset", (bits& 2)==0);
+      MyConfig.SetParamValueBool("xrt", "kickdown",  (bits& 4)==0);
+      MyConfig.SetParamValueBool("xrt", "autopower", (bits& 8)==0);
+      MyConfig.SetParamValueBool("xrt", "console",   (bits&16)!=0);
       return true;
     }
     case 16:
-      MyConfig.SetParamValue("x.rt", "cap_nom_ah", value);
+      MyConfig.SetParamValue("xrt", "cap_nom_ah", value);
       return true;
     default:
       return OvmsVehicle::SetFeature(key, value);
@@ -223,37 +223,37 @@ const std::string OvmsVehicleRenaultTwizy::GetFeature(int key)
   switch (key)
   {
     case 0:
-      return MyConfig.GetParamValue("x.rt", "gpslogint", XSTR(0));
+      return MyConfig.GetParamValue("xrt", "gpslogint", XSTR(0));
     case 1:
-      return MyConfig.GetParamValue("x.rt", "kd_threshold", XSTR(CFG_DEFAULT_KD_THRESHOLD));
+      return MyConfig.GetParamValue("xrt", "kd_threshold", XSTR(CFG_DEFAULT_KD_THRESHOLD));
     case 2:
-      return MyConfig.GetParamValue("x.rt", "kd_compzero", XSTR(CFG_DEFAULT_KD_COMPZERO));
+      return MyConfig.GetParamValue("xrt", "kd_compzero", XSTR(CFG_DEFAULT_KD_COMPZERO));
     case 6:
-      return MyConfig.GetParamValue("x.rt", "chargemode", XSTR(0));
+      return MyConfig.GetParamValue("xrt", "chargemode", XSTR(0));
     case 7:
-      return MyConfig.GetParamValue("x.rt", "chargelevel", XSTR(0));
+      return MyConfig.GetParamValue("xrt", "chargelevel", XSTR(0));
     case 10:
-      return MyConfig.GetParamValue("x.rt", "suffsoc", XSTR(0));
+      return MyConfig.GetParamValue("xrt", "suffsoc", XSTR(0));
     case 11:
-      return MyConfig.GetParamValue("x.rt", "suffrange", XSTR(0));
+      return MyConfig.GetParamValue("xrt", "suffrange", XSTR(0));
     case 12:
-      return MyConfig.GetParamValue("x.rt", "maxrange", XSTR(CFG_DEFAULT_MAXRANGE));
+      return MyConfig.GetParamValue("xrt", "maxrange", XSTR(CFG_DEFAULT_MAXRANGE));
     case 13:
-      return MyConfig.GetParamValue("x.rt", "cap_act_prc", XSTR(100));
+      return MyConfig.GetParamValue("xrt", "cap_act_prc", XSTR(100));
     case 15:
     {
       int bits =
-        ( MyConfig.GetParamValueBool("x.rt", "canwrite",  false) ?  1 : 0) |
-        (!MyConfig.GetParamValueBool("x.rt", "autoreset", true)  ?  2 : 0) |
-        (!MyConfig.GetParamValueBool("x.rt", "kickdown",  true)  ?  4 : 0) |
-        (!MyConfig.GetParamValueBool("x.rt", "autopower", true)  ?  8 : 0) |
-        ( MyConfig.GetParamValueBool("x.rt", "console",   false) ? 16 : 0);
+        ( MyConfig.GetParamValueBool("xrt", "canwrite",  false) ?  1 : 0) |
+        (!MyConfig.GetParamValueBool("xrt", "autoreset", true)  ?  2 : 0) |
+        (!MyConfig.GetParamValueBool("xrt", "kickdown",  true)  ?  4 : 0) |
+        (!MyConfig.GetParamValueBool("xrt", "autopower", true)  ?  8 : 0) |
+        ( MyConfig.GetParamValueBool("xrt", "console",   false) ? 16 : 0);
       char buf[4];
       sprintf(buf, "%d", bits);
       return std::string(buf);
     }
     case 16:
-      return MyConfig.GetParamValue("x.rt", "cap_nom_ah", XSTR(CFG_DEFAULT_CAPACITY));
+      return MyConfig.GetParamValue("xrt", "cap_nom_ah", XSTR(CFG_DEFAULT_CAPACITY));
     default:
       return OvmsVehicle::GetFeature(key);
   }
@@ -1120,7 +1120,7 @@ void OvmsVehicleRenaultTwizy::Ticker1(uint32_t ticker)
           cfg_bat_cap_actual_prc = new_cap_prc;
           
           // store in config flash:
-          MyConfig.SetParamValueFloat("x.rt", "cap_act_prc", cfg_bat_cap_actual_prc);
+          MyConfig.SetParamValueFloat("xrt", "cap_act_prc", cfg_bat_cap_actual_prc);
         }
 
       }
