8e091ebbfc5b614cca9ac1e2e9402fb284f31ef6
Mark Webb-Johnson <mark@webb-johnson.net>
2018-01-17 15:59:15 +0800

    Hierarchial config for OVMS server selection

--- vehicle/OVMS.V3/components/ovms_server_v2/component.mk
+++ vehicle/OVMS.V3/components/ovms_server_v2/component.mk
@@ -7,8 +7,10 @@
 # please read the ESP-IDF documents if you need to do this.
 #
 
+ifdef CONFIG_OVMS_COMP_SERVER
 ifdef CONFIG_OVMS_COMP_SERVER_V2
 COMPONENT_SRCDIRS := src
 COMPONENT_ADD_INCLUDEDIRS := src
 COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
+endif
 endif
