f9e4fe342f362e194b547a03237dcd8ed70c0912
Mark Webb-Johnson <mark@webb-johnson.net>
2018-03-19 09:01:00 +0800

    ServerV3: Tidy up connect/disconnect, via network manager events

--- vehicle/OVMS.V3/components/ovms_server_v3/src/ovms_server_v3.cpp
+++ vehicle/OVMS.V3/components/ovms_server_v3/src/ovms_server_v3.cpp
@@ -331,22 +331,33 @@ void OvmsServerV3::ConfigChanged(OvmsConfigParam* param)
 
 void OvmsServerV3::NetUp(std::string event, void* data)
   {
+  // workaround for wifi AP mode startup (manager up before interface)
+  if ( (m_mgconn == NULL) && MyNetManager.MongooseRunning() )
+    {
+    ESP_LOGI(TAG, "Network is up, so attempt network connection");
+    Connect(); // Kick off the connection
+    }
   }
 
 void OvmsServerV3::NetDown(std::string event, void* data)
   {
+  if (m_mgconn)
+    {
+    ESP_LOGI(TAG, "Network is down, so disconnect network connection");
+    Disconnect();
+    }
   }
 
 void OvmsServerV3::NetReconfigured(std::string event, void* data)
   {
-  ESP_LOGI(TAG, "Network is reconfigured, so disconnect network connection");
+  ESP_LOGI(TAG, "Network is reconfigured, so disconnect network connection (and reconnect in 10 seconds)");
   Disconnect();
   m_connretry = 10;
   }
 
 void OvmsServerV3::NetmanInit(std::string event, void* data)
   {
-  if (m_mgconn == NULL)
+  if ((m_mgconn == NULL)&&(MyNetManager.m_connected_any))
     {
     ESP_LOGI(TAG, "Network is up, so attempt network connection");
     Connect(); // Kick off the connection
