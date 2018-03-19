17a528bbe4e021768e28251984ad4532e9b745bb
Mark Webb-Johnson <mark@webb-johnson.net>
2018-03-19 11:25:13 +0800

    ovms_mutex facility library

---
+++ vehicle/OVMS.V3/main/ovms_mutex.cpp
@@ -0,0 +1,67 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          14th March 2017
+;
+;    Changes:
+;    1.0  Initial release
+;
+;    (C) 2011       Michael Stegen / Stegen Electronics
+;    (C) 2011-2017  Mark Webb-Johnson
+;    (C) 2011        Sonny Chen @ EPRO/DX
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#include "ovms_mutex.h"
+
+OvmsMutex::OvmsMutex()
+  {
+  m_mutex = xSemaphoreCreateMutex();
+  }
+
+OvmsMutex::~OvmsMutex()
+  {
+  vSemaphoreDelete(m_mutex);
+  }
+
+bool OvmsMutex::Lock(TickType_t timeout)
+  {
+  return (xSemaphoreTake(m_mutex, timeout) == pdTRUE);
+  }
+
+void OvmsMutex::Unlock()
+  {
+  xSemaphoreGive(m_mutex);
+  }
+
+OvmsMutexLock::OvmsMutexLock(OvmsMutex* mutex, TickType_t timeout)
+  {
+  m_mutex = mutex;
+  m_locked = m_mutex->Lock(timeout);
+  }
+
+OvmsMutexLock::~OvmsMutexLock()
+  {
+  if (m_locked) m_mutex->Unlock();
+  }
+
+bool OvmsMutexLock::IsLocked()
+  {
+  return m_locked;
+  }
