aeb7728b003d208fbd08bb3577148c115f45a7e5
Mark Webb-Johnson <mark@webb-johnson.net>
2018-04-11 07:50:22 +0800

    Tesla Model S: Refinements for v.e.handbrake and Mph/Kph support

--- vehicle/OVMS.V3/components/vehicle_teslamodels/src/vehicle_teslamodels.cpp
+++ vehicle/OVMS.V3/components/vehicle_teslamodels/src/vehicle_teslamodels.cpp
@@ -67,21 +67,25 @@ void OvmsVehicleTeslaModelS::IncomingFrameCan1(CAN_frame_t* p_frame)
           StandardMetrics.ms_v_env_gear->SetValue(0);
           StandardMetrics.ms_v_env_on->SetValue(false);
           StandardMetrics.ms_v_env_awake->SetValue(false);
+          StandardMetrics.ms_v_env_handbrake->SetValue(true);
           break;
         case 2: // Reverse
           StandardMetrics.ms_v_env_gear->SetValue(-1);
           StandardMetrics.ms_v_env_on->SetValue(true);
           StandardMetrics.ms_v_env_awake->SetValue(true);
+          StandardMetrics.ms_v_env_handbrake->SetValue(false);
           break;
         case 3: // Neutral
           StandardMetrics.ms_v_env_gear->SetValue(0);
           StandardMetrics.ms_v_env_on->SetValue(true);
           StandardMetrics.ms_v_env_awake->SetValue(true);
+          StandardMetrics.ms_v_env_handbrake->SetValue(false);
           break;
         case 4: // Drive
           StandardMetrics.ms_v_env_gear->SetValue(1);
           StandardMetrics.ms_v_env_on->SetValue(true);
           StandardMetrics.ms_v_env_awake->SetValue(true);
+          StandardMetrics.ms_v_env_handbrake->SetValue(false);
           break;
         default:
           break;
@@ -90,7 +94,7 @@ void OvmsVehicleTeslaModelS::IncomingFrameCan1(CAN_frame_t* p_frame)
       }
     case 0x256: // Speed
       {
-      StandardMetrics.ms_v_pos_speed->SetValue( ((((int)d[3]&0x0f)<<8) + (int)d[2])/10, Mph );
+      StandardMetrics.ms_v_pos_speed->SetValue( ((((int)d[3]&0x0f)<<8) + (int)d[2])/10, (d[3]&0x80)?Kph:Mph );
       break;
       }
     case 0x302: // SOC
