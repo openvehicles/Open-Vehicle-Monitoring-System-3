6f7fe15c768f66f33d6ac33dcaaefe9681b34d76
Paul <paul@rensel.nl>
2018-07-17 22:08:48 +0200

    Added initial support for Zeva BMS system

---
+++ vehicle/OVMS.V3/components/vehicle_zeva/src/vehicle_zeva.h
@@ -0,0 +1,65 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          17 July 2018
+;
+;    Changes:
+;    1.1  Modified for Zeva BMS
+;    1.0  Initial release
+;
+;    (C) 2018	    Paul Rensel / Reho Engineering
+;    (C) 2011       Michael Stegen / Stegen Electronics
+;    (C) 2011-2017  Mark Webb-Johnson
+;    (C) 2011        Sonny Chen @ EPRO/DX
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#ifndef __VEHICLE_ZEVA_H__
+#define __VEHICLE_ZEVA_H__
+
+#include "vehicle.h"
+
+using namespace std;
+
+class OvmsVehicleZeva : public OvmsVehicle
+  {
+  public:
+    OvmsVehicleZeva();
+    ~OvmsVehicleZeva();
+
+  public:
+    virtual void Ticker1(uint32_t ticker);
+    virtual void Ticker10(uint32_t ticker);
+
+  public:
+    virtual vehicle_command_t CommandSetChargeMode(vehicle_mode_t mode);
+    virtual vehicle_command_t CommandSetChargeCurrent(uint16_t limit);
+    virtual vehicle_command_t CommandStartCharge();
+    virtual vehicle_command_t CommandStopCharge();
+    virtual vehicle_command_t CommandSetChargeTimer(bool timeron, uint16_t timerstart);
+    virtual vehicle_command_t CommandCooldown(bool cooldownon);
+    virtual vehicle_command_t CommandWakeup();
+    virtual vehicle_command_t CommandLock(const char* pin);
+    virtual vehicle_command_t CommandUnlock(const char* pin);
+    virtual vehicle_command_t CommandActivateValet(const char* pin);
+    virtual vehicle_command_t CommandDeactivateValet(const char* pin);
+    virtual vehicle_command_t CommandHomelink(int button, int durationms=1000);
+  };
+
+#endif //#ifndef __VEHICLE_ZEVA_H__
