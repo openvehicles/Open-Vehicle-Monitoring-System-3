8b589568492f1ebd7c1a99d34c44dde7129b4475
Mark Webb-Johnson <mark@webb-johnson.net>
2018-11-06 22:45:08 +0800

    TeslaModelS: BMS 0x6f2 decode - brick voltages and module temperatures

--- vehicle/OVMS.V3/components/vehicle_teslamodels/src/vehicle_teslamodels.cpp
+++ vehicle/OVMS.V3/components/vehicle_teslamodels/src/vehicle_teslamodels.cpp
@@ -38,21 +38,94 @@ static const char *TAG = "v-teslamodels";
 #include "ovms_metrics.h"
 #include "metrics_standard.h"
 
+void vehicle_teslamodels_bms(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc, const char* const* argv)
+  {
+  OvmsVehicleTeslaModelS* models = (OvmsVehicleTeslaModelS*) MyVehicleFactory.ActiveVehicle();
+  string type = StdMetrics.ms_v_type->AsString();
+
+  if (!models || type.substr(0,2) != "TS")
+    {
+    writer->puts("Error: Tesla Model S vehicle module not selected");
+    return;
+    }
+
+  models->CommandBMS(verbosity, writer, cmd, argc, argv);
+  }
+
 OvmsVehicleTeslaModelS::OvmsVehicleTeslaModelS()
   {
   ESP_LOGI(TAG, "Tesla Model S vehicle module");
 
   memset(m_vin,0,sizeof(m_vin));
   memset(m_type,0,sizeof(m_type));
+  memset(&m_brick_v,0,sizeof(m_brick_v));
+  memset(&m_module_t1,0,sizeof(m_module_t1));
+  memset(&m_module_t2,0,sizeof(m_module_t2));
+  m_bmvt = 0;
 
   RegisterCanBus(1,CAN_MODE_ACTIVE,CAN_SPEED_500KBPS);
   RegisterCanBus(2,CAN_MODE_ACTIVE,CAN_SPEED_500KBPS);
   RegisterCanBus(3,CAN_MODE_ACTIVE,CAN_SPEED_125KBPS);
+
+  m_bms_v = new OvmsMetricVector<float>("xts.b.brick.voltages", SM_STALE_MIN, Volts);
+  m_bms_t1 = new OvmsMetricVector<float>("xts.b.module.temps1", SM_STALE_MIN, Celcius);
+  m_bms_t2 = new OvmsMetricVector<float>("xts.b.module.temps2", SM_STALE_MIN, Celcius);
+
+  OvmsCommand* cmd_xts = MyCommandApp.RegisterCommand("xts", "Tesla Model S", NULL, "", 0, 0, true);
+  cmd_xts->RegisterCommand("bms", "BMS status", vehicle_teslamodels_bms, "", 0, 0, true);
   }
 
 OvmsVehicleTeslaModelS::~OvmsVehicleTeslaModelS()
   {
   ESP_LOGI(TAG, "Shutdown Tesla Model S vehicle module");
+  }
+
+void OvmsVehicleTeslaModelS::CommandBMS(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc, const char* const* argv)
+  {
+  float tmin = 1000;
+  float tmax = -1000;
+  float vmin = 1000;
+  float vmax = 0;
+  float vtot = 0;
+
+  if (m_bmvt == 0)
+    {
+    writer->puts("No BMS status data available");
+    return;
+    }
+
+  for (int mod=0;mod<16;mod++)
+    {
+    if (m_module_t1[mod]<tmin) tmin = m_module_t1[mod];
+    if (m_module_t2[mod]<tmin) tmin = m_module_t2[mod];
+    if (m_module_t1[mod]>tmax) tmax = m_module_t1[mod];
+    if (m_module_t2[mod]>tmax) tmax = m_module_t2[mod];
+    }
+
+  writer->puts("   Tesla Model S BMS status");
+  writer->puts("   -------------------------------");
+  for (int mod=0;mod<16;mod++)
+    {
+    int bo = mod*6;
+    for (int j=bo;j<bo+6;j++)
+      {
+      if (m_brick_v[j]<vmin) vmin = m_brick_v[j];
+      if (m_brick_v[j]>vmax) vmax = m_brick_v[j];
+      vtot += m_brick_v[j];
+      }
+    writer->printf("%2d | %4.3f V | %4.3f V | %4.3f V | %3.1f C %s%s\n",
+      mod+1, m_brick_v[bo], m_brick_v[bo+1], m_brick_v[bo+2], m_module_t1[mod],
+      (m_module_t1[mod]<=tmin)?"Min":"",
+      (m_module_t1[mod]>=tmax)?"Max":"");
+    writer->printf("   | %4.3f V | %4.3f V | %4.3f V | %3.1f C %s%s\n",
+      m_brick_v[bo+3], m_brick_v[bo+4], m_brick_v[bo+5], m_module_t2[mod],
+      (m_module_t2[mod]<=tmin)?"Min":"",
+      (m_module_t2[mod]>=tmax)?"Max":"");
+    writer->puts("   -------------------------------");
+    }
+
+  writer->printf("   Tmin: %3.1f  Tmax: %3.1f  Vmax: %4.3f  Vmin: %4.3f  Vmax-Vmin: %4.3f  Vtot: %5.2f\n",
+    tmin, tmax, vmax, vmin, vmax-vmin, vtot);
   }
 
 void OvmsVehicleTeslaModelS::IncomingFrameCan1(CAN_frame_t* p_frame)
@@ -155,6 +228,43 @@ void OvmsVehicleTeslaModelS::IncomingFrameCan1(CAN_frame_t* p_frame)
                                                 + ((uint32_t)d[2]<<16)
                                                 + ((uint32_t)d[1]<<8)
                                                 + d[0])/1000, Miles);
+      break;
+      }
+    case 0x6f2: // BMS brick voltage and module temperatures
+      {
+      if (m_bmvt == 0xffffffff)
+        {
+        m_bms_v->SetElemValues(0, 96, m_brick_v);
+        m_bms_t1->SetElemValues(0, 16, m_module_t1);
+        m_bms_t2->SetElemValues(0, 16, m_module_t2);
+        m_bmvt = (1 << d[0]);
+        }
+      else
+        {
+        m_bmvt |= (1 << d[0]);
+        }
+      int v1 = ((int)(d[2]&0x3f)<<8) + d[1];
+      int v2 = (((int)d[4]&0x0f)<<10) + (((int)d[3])<<2) + (d[2]>>6);
+      int v3 = (((int)d[6]&0x03)<<12) + (((int)d[5])<<4) + (d[4]>>4);
+      int v4 = (((int)d[7])<<6) + (((int)d[6]>>2));
+      if (d[0]<24)
+        {
+        // Voltages
+        int k = d[0]*4;
+        m_brick_v[k] = 0.000305 * v1;
+        m_brick_v[k+1] = 0.000305 * v2;
+        m_brick_v[k+2] = 0.000305 * v3;
+        m_brick_v[k+3] = 0.000305 * v4;
+        }
+      else
+        {
+        // Temperatures
+        int k = (d[0]-24)*2;
+        m_module_t1[k] = 0.0122 * ((v1 & 0x1FFF) - (v1 & 0x2000));
+        m_module_t2[k] = 0.0122 * ((v2 & 0x1FFF) - (v2 & 0x2000));
+        m_module_t1[k+1] = 0.0122 * ((v3 & 0x1FFF) - (v3 & 0x2000));
+        m_module_t2[k+1] = 0.0122 * ((v4 & 0x1FFF) - (v4 & 0x2000));
+        }
       break;
       }
     default:
--- vehicle/OVMS.V3/components/vehicle_teslamodels/src/vehicle_teslamodels.h
+++ vehicle/OVMS.V3/components/vehicle_teslamodels/src/vehicle_teslamodels.h
@@ -32,6 +32,8 @@
 #define __VEHICLE_TESLAMODELS_H__
 
 #include "vehicle.h"
+#include "ovms_command.h"
+#include "ovms_metrics.h"
 
 using namespace std;
 
@@ -42,6 +44,7 @@ class OvmsVehicleTeslaModelS: public OvmsVehicle
     ~OvmsVehicleTeslaModelS();
 
   public:
+    void CommandBMS(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc, const char* const* argv);
     void IncomingFrameCan1(CAN_frame_t* p_frame);
     void IncomingFrameCan2(CAN_frame_t* p_frame);
     void IncomingFrameCan3(CAN_frame_t* p_frame);
@@ -53,6 +56,13 @@ class OvmsVehicleTeslaModelS: public OvmsVehicle
   protected:
     char m_vin[18];
     char m_type[5];
+    float m_brick_v[96];
+    float m_module_t1[16];
+    float m_module_t2[16];
+    uint32_t m_bmvt;
+    OvmsMetricVector<float>* m_bms_v;
+    OvmsMetricVector<float>* m_bms_t1;
+    OvmsMetricVector<float>* m_bms_t2;
   };
 
 #endif //#ifndef __VEHICLE_TESLAMODELS_H__
