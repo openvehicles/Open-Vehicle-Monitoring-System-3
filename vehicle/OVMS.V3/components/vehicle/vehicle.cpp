976002f8ae4c11603238789c858d74f7b1f8158c
Stephen Casner <casner@acm.org>
2019-01-18 15:27:07 -0800

    Fix a typo

--- vehicle/OVMS.V3/components/vehicle/vehicle.cpp
+++ vehicle/OVMS.V3/components/vehicle/vehicle.cpp
@@ -335,7 +335,7 @@ void vehicle_charge_current(int verbosity, OvmsWriter* writer, OvmsCommand* cmd,
       writer->printf("Charge current limit set to %dA\n",limit);
       break;
     case OvmsVehicle::Fail:
-      writer->printf("Error: Could not sst charge current limit to %dA\n",limit);
+      writer->printf("Error: Could not set charge current limit to %dA\n",limit);
       break;
     default:
       writer->puts("Error: Charge current limit functionality not available");
