55fc96456cc486438607babc092ec378a681b2b8
devmarxx <devmarxx@m-scene.de>
2020-02-04 22:07:35 +0100

    neue Datei:     vehicle/OVMS.V3/components/vehicle_vweup/component.mk 	neue Datei:     vehicle/OVMS.V3/components/vehicle_vweup/docs/index.rst 	neue Datei:     vehicle/OVMS.V3/components/vehicle_vweup/src/vehicle_vweup.cpp 	neue Datei:     vehicle/OVMS.V3/components/vehicle_vweup/src/vehicle_vweup.h 	geändert:       vehicle/OVMS.V3/main/Kconfig

---
+++ vehicle/OVMS.V3/components/vehicle_vweup/docs/index.rst
@@ -0,0 +1,60 @@
+=======
+VW e-Up 
+=======
+
+Vehicle Type: **VWUP**
+
+This vehicle type supports the VW e-UP, Skoda Citigo E IV and the Seat MII electric.
+
+----------------
+Support Overview
+----------------
+
+=========================== ==============
+Function                    Support Status
+=========================== ==============
+Hardware                    Any OVMS v3 (or later) module. Vehicle support: 2020- (should support 2013- VW e-Up as well)
+Vehicle Cable               Comfort CAN T26A (OCU connector cable, located under front passenger seat) to DB9 Data Cable for OVMS using pin 6 and 8 for can3
+GSM Antenna                 T4AC - R205 with fakra_sma adapter cable or 1000500 Open Vehicles OVMS GSM Antenna (or any compatible antenna)
+GPS Antenna                 T4AC - R50 with fakra_sma adapter cable or 1020200 Universal GPS Antenna (or any compatible antenna)
+SOC Display                 tba
+Range Display               tba
+Cabin Pre-heat/cool Control tba
+GPS Location                tba
+Speed Display               tba
+Temperature Display         tba
+BMS v+t Display             tba
+TPMS Display                tba
+Charge Status Display       tba
+Charge Interruption Alerts  tba
+Charge Control              tba
+Lock/Unlock Vehicle         tba
+Valet Mode Control          tba
+Others                      tba
+=========================== ==============
+
+----------------------------------------
+Pinout OCU T4AC - OVMS DB9 adapter cable
+----------------------------------------
+
+OCU	DB9-F	Signal
+-----------------------------------
+26	3	Chassis / Power GND
+	2	can1 L (Can Low, not used)
+	7	can1 H (Can High, not used)
+	4	can2 L (Can Low, not used)
+	5	can2 H (Can High, not used)
+2	6	can3 L (Comfort-can Low)
+14	8	can3 H (Comfort-can High)
+1	9	+12V Vehicle Power
+
+Because of the compilicated VAG OBD-gateway communication protocol
+we directly tap into the comfort can bus over the OCU cable.
+
+Advantage is a faster and easier development.
+
+Disadvantage is that we won't have all vehicle information available
+and we won't be able to access all control units.
+
+====================================================================
+
---
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/vehicle_vweup.cpp
@@ -0,0 +1,68 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          5th July 2018
+;
+;    Changes:
+;    1.0  Initial release
+;
+;    (C) 2011       Michael Stegen / Stegen Electronics
+;    (C) 2011-2018  Mark Webb-Johnson
+;    (C) 2011       Sonny Chen @ EPRO/DX
+;    (C) 2020       Chris van der Meijden
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#include "ovms_log.h"
+static const char *TAG = "v-vweup";
+
+#include <stdio.h>
+#include "vehicle_vweup.h"
+
+OvmsVehicleVWeUP::OvmsVehicleVWeUP()
+  {
+  ESP_LOGI(TAG, "Start VW e-Up vehicle module");
+
+  RegisterCanBus(3,CAN_MODE_ACTIVE,CAN_SPEED_100KBPS);
+  }
+
+OvmsVehicleVWeUP::~OvmsVehicleVWeUP()
+  {
+  ESP_LOGI(TAG, "Stop VW e-Up vehicle module");
+  }
+
+void OvmsVehicleVWeUP::IncomingFrameCan1(CAN_frame_t* p_frame)
+  {
+  }
+
+void OvmsVehicleVWeUP::Ticker1(uint32_t ticker)
+  {
+  }
+
+class OvmsVehicleVWeUPInit
+  {
+  public: OvmsVehicleVWeUPInit();
+} OvmsVehicleVWeUPInit  __attribute__ ((init_priority (9000)));
+
+OvmsVehicleVWeUPInit::OvmsVehicleVWeUPInit()
+  {
+  ESP_LOGI(TAG, "Registering Vehicle: VW e-Up (9000)");
+
+  MyVehicleFactory.RegisterVehicle<OvmsVehicleVWeUP>("VWUP","VW e-Up");
+  }
