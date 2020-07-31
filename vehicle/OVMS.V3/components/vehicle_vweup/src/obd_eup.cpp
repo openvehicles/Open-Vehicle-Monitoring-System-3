759fb4bb849fee9d2116bdcbfb6654a6175312a5
SokoFromNZ <soko17@soko.cc>
2020-07-31 12:42:16 +0200

    VWeUp-OBD: Poll of SoC

--- vehicle/OVMS.V3/components/vehicle_vweup/src/obd_eup.cpp
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/obd_eup.cpp
@@ -10,25 +10,43 @@ static const char *TAG = "vwup.obd";
 #include "ovms_events.h"
 #include "ovms_metrics.h"
 
-
 // Last array (polltime) is for max. 4 different poll-states. I only use State=0 at the moment
-const OvmsVehicle::poll_pid_t vwup_poll[] = {
-  // Note: poller ticker cycles at 3600 seconds = max period
-  // { txid, rxid, type, pid, { period_off, period_drive, period_charge } }
-  { 0x7E5, 0x7ED, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x028C, { 0, 10, 60 } },
-  { 0, 0, 0, 0, { 0, 0, 0 } }
-};
+const OvmsVehicle::poll_pid_t vwup_polls[] = {
+    // Note: poller ticker cycles at 3600 seconds = max period
+    // { txid, rxid, type, pid, { period_off, period_drive, period_charge } }
+    {VWUP_BAT_MGMT_RX, VWUP_BAT_MGMT_TX, VEHICLE_POLL_TYPE_OBDIIEXTENDED, VWUP_BAT_MGMT_SOC, {10, 10, 10}},
+    {0, 0, 0, 0, {0, 0, 0}}};
 
 VWeUpObd::VWeUpObd()
 {
   ESP_LOGI(TAG, "Start VW e-Up OBD vehicle module");
-  
+
   // init configs:
   MyConfig.RegisterParam("vwup", "VW e-Up", true, true);
   ConfigChanged(NULL);
+
+  // init polls:
+  RegisterCanBus(1, CAN_MODE_ACTIVE, CAN_SPEED_500KBPS);
+  PollSetPidList(m_can1, vwup_polls);
+  PollSetState(0);
 }
 
 VWeUpObd::~VWeUpObd()
 {
   ESP_LOGI(TAG, "Stop VW e-Up OBD vehicle module");
+}
+
+void VWeUpObd::IncomingPollReply(canbus *bus, uint16_t type, uint16_t pid, uint8_t *data, uint8_t length, uint16_t remain)
+{
+  ESP_LOGD(TAG, "IncomingPollReply(type=%u, pid=%u, length=%u, remain=%u): called", type, pid, length, remain);
+
+  switch (pid)
+  {
+  case VWUP_BAT_MGMT_SOC:
+    uint8_t value = *data;
+    float floatSoC = (float)((short)value) / 2.55f;
+    ESP_LOGV(TAG, "IncomingPollReply(): pid=%X value=%u floatSoC=%f", (short)pid, value, floatSoC);
+    StandardMetrics.ms_v_bat_soc->SetValue(floatSoC);
+    break;
+  }
 }
--- vehicle/OVMS.V3/components/vehicle_vweup/src/obd_eup.h
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/obd_eup.h
@@ -3,11 +3,20 @@
 
 #include "vehicle_vweup.h"
 
+// ECUs TX/RX
+#define VWUP_BAT_MGMT_TX 0x7E5
+#define VWUP_BAT_MGMT_RX 0x7ED
+
+// PIDs of ECUs
+#define VWUP_BAT_MGMT_SOC 0x028C
+
 class VWeUpObd : public OvmsVehicle
 {
 public:
   VWeUpObd();
   ~VWeUpObd();
+  
+  void IncomingPollReply(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain);
 
 private:
 
