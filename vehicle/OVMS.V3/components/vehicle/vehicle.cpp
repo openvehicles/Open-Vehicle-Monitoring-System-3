90033e5bd5203cd5905e3f79b8052469e66f2f34
SokoFromNZ <soko17@soko.cc>
2020-08-22 12:50:49 +0200

    Correct handling of Data and Remain

--- vehicle/OVMS.V3/components/vehicle/vehicle.cpp
+++ vehicle/OVMS.V3/components/vehicle/vehicle.cpp
@@ -2244,11 +2244,7 @@ void OvmsVehicle::PollerReceive(CAN_frame_t* frame)
           (frame->data.u8[2] == 0x40+m_poll_type)&&
           ((frame->data.u8[4]+(((uint16_t) frame->data.u8[3]) << 8))  == m_poll_pid))
         {
-        // First frame is 4 bytes header (2 ISO-TP, 2 OBDII), 4 bytes data:
-        // [first=1,lenH] [lenL] [type+40] [pid] [data0] [data1] [data2] [data3]
-        // Note that the value of 'len' includes the OBDII type and pid bytes,
-        // but we don't count these in the data we pass to IncomingPollReply.
-        //
+
         // First frame; send flow control frame:
         CAN_frame_t txframe;
         memset(&txframe,0,sizeof(txframe));
@@ -2270,16 +2266,16 @@ void OvmsVehicle::PollerReceive(CAN_frame_t* frame)
 
         txframe.data.u8[0] = 0x30; // flow control frame type
         txframe.data.u8[1] = 0x00; // request all frames available
-        txframe.data.u8[2] = 0x19; // with 25ms send interval
+        txframe.data.u8[2] = 0x01; // with 1 ms send interval (as fast as possible)
         txframe.Write();
 
-        // prepare frame processing, first frame contains first 4 bytes:
-        m_poll_ml_remain = (((uint16_t)(frame->data.u8[0]&0x0f))<<8) + frame->data.u8[1] - 3;
+        // prepare frame processing, first frame contains three data bytes (-3) and value of u8[1] counts the two PID bytes as well (-2)
+        m_poll_ml_remain = (uint16_t)(frame->data.u8[1] - 3 - 2);
         m_poll_ml_offset = 3;
         m_poll_ml_frame = 0;
 
         //ESP_LOGD(TAG, "Poll ML first frame (frame=%d, remain=%d)",m_poll_ml_frame,m_poll_ml_remain);
-        IncomingPollReplyInternal(frame->origin, m_poll_type, m_poll_pid, &frame->data.u8[4], 4, m_poll_ml_remain);
+        IncomingPollReplyInternal(frame->origin, m_poll_type, m_poll_pid, &frame->data.u8[5], 3, m_poll_ml_remain);
         return;
         }
       else if (((frame->data.u8[0]>>4)==0x2)&&(m_poll_ml_remain>0))
