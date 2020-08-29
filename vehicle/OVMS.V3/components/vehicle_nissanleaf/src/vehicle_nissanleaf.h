7d4df5f8b9dd1331df45ebd2605d79f8cd994ba4
drc38 <derek_Caudwell@hotmail.com>
2020-08-29 20:49:20 +1200

    Revert "NL: set RespSepTime to 0, add poll once function"
    This reverts commit 99207ca5d9ad67b16c25302f9361fa5d2627b7a2.

--- vehicle/OVMS.V3/components/vehicle_nissanleaf/src/vehicle_nissanleaf.h
+++ vehicle/OVMS.V3/components/vehicle_nissanleaf/src/vehicle_nissanleaf.h
@@ -122,7 +122,7 @@ class OvmsVehicleNissanLeaf : public OvmsVehicle
     void GetDashboardConfig(DashboardConfig& cfg);
 
   private:
-    void CommandInit(); // initialise shell commands specific to Leaf
+    void CommandInit();
     void vehicle_nissanleaf_car_on(bool isOn);
     void vehicle_nissanleaf_charger_status(ChargerStatus status);
     void SendCanMessage(uint16_t id, uint8_t length, uint8_t *data);
@@ -145,7 +145,7 @@ class OvmsVehicleNissanLeaf : public OvmsVehicle
     void PollReply_VIN(uint8_t reply_data[], uint16_t reply_len);
     void PollReply_BMS_Volt(uint8_t reply_data[], uint16_t reply_len);
     void PollReply_BMS_Temp(uint8_t reply_data[], uint16_t reply_len);
-    void PollOnceRequests(); // function to get obd pid data that only needs to be obtained once
+    void PollSetBus(canbus* bus);
 
     TimerHandle_t m_remoteCommandTimer;
     TimerHandle_t m_ccDisableTimer;
