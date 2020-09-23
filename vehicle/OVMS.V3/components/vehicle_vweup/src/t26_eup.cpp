3572081a56a510dc660f74a23e5381281c0affc5
devmarxx <devmarxx@m-scene.de>
2020-09-23 15:11:44 +0200

    Corrected Version date
    Changed:
    vehicle/OVMS.V3/components/vehicle_vweup/src/t26_eup.cpp

--- vehicle/OVMS.V3/components/vehicle_vweup/src/t26_eup.cpp
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/t26_eup.cpp
@@ -31,7 +31,7 @@
 
 /*
 ;    Subproject:    Integration of support for the VW e-UP
-;    Date:          14st September 2020
+;    Date:          23rd September 2020
 ;
 ;    Changes:
 ;    0.1.0  Initial code
