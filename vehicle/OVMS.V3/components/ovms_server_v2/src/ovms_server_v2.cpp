d26e9a8b6c24d4d64fd102336aaf3335e850c0d1
sharkcow <40037034+sharkcow@users.noreply.github.com>
2020-12-07 20:59:57 +0100

    change charge current to float
    otherwise the decimals in the android app aren't of any use :)

--- vehicle/OVMS.V3/components/ovms_server_v2/src/ovms_server_v2.cpp
+++ vehicle/OVMS.V3/components/ovms_server_v2/src/ovms_server_v2.cpp
@@ -1002,7 +1002,7 @@ void OvmsServerV2::TransmitMsgStat(bool always)
     << ","
     << StandardMetrics.ms_v_charge_voltage->AsInt()
     << ","
-    << StandardMetrics.ms_v_charge_current->AsInt()
+    << StandardMetrics.ms_v_charge_current->AsString("0", Other, 1)
     << ","
     << StandardMetrics.ms_v_charge_state->AsString("stopped")
     << ","
