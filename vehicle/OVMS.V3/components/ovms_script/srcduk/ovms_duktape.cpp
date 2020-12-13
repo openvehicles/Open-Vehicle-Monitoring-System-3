49a0866a68895c3b78b608d4f3b81c31bf8184e3
Michael Balzer <balzer@expeedo.de>
2020-12-13 13:27:06 +0100

    Script/Duktape: log garbage collection times (ported to for-v3.3)

--- vehicle/OVMS.V3/components/ovms_script/srcduk/ovms_duktape.cpp
+++ vehicle/OVMS.V3/components/ovms_script/srcduk/ovms_duktape.cpp
@@ -1350,9 +1350,11 @@ void OvmsDuktape::DukTapeTask()
           // Compact DUKTAPE memory
           if (m_dukctx != NULL)
             {
-            ESP_LOGD(TAG,"Duktape: Compacting DukTape memory");
+            ESP_LOGV(TAG,"Duktape: Compacting DukTape memory");
+            uint32_t ts = esp_log_timestamp();
             duk_gc(m_dukctx, 0);
             duk_gc(m_dukctx, 0);
+            ESP_LOGD(TAG, "Duktape: Compacting DukTape memory done in %u ms", esp_log_timestamp()-ts);
             }
           }
           break;
