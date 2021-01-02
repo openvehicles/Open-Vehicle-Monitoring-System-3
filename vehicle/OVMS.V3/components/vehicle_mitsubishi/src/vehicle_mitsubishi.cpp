7a52f1c104eea6431c0733b2ab6abd4e70157e6f
Phil Sutherland <phils@rct-global.com>
2021-01-02 18:47:20 +0800

    Add old iMIEV key status sentence
    Older iMIEV appears to use 0x012 byte 2 instead of 0x101 byte 0 for key status information.  This change assumes both sentences will not be seen on the same iMIEV.

--- vehicle/OVMS.V3/components/vehicle_mitsubishi/src/vehicle_mitsubishi.cpp
+++ vehicle/OVMS.V3/components/vehicle_mitsubishi/src/vehicle_mitsubishi.cpp
@@ -239,7 +239,13 @@ void OvmsVehicleMitsubishi::IncomingFrameCan1(CAN_frame_t* p_frame)
 
   switch ( p_frame->MsgID )
     {
-      case 0x101: //freq10 //Key status
+      case 0x012: // Key status (early iMIEV)
+      {
+        (d[2] == 4) ? vehicle_mitsubishi_car_on(true) : vehicle_mitsubishi_car_on(false);
+      break;
+      }
+      
+      case 0x101: //freq10 //Key status (later iMIEV)
       {
         (d[0] == 4) ? vehicle_mitsubishi_car_on(true) : vehicle_mitsubishi_car_on(false);
       break;
