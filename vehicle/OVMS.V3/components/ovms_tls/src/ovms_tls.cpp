183d645c80bf199c49a49f852cd2f297fdc727f4
Michael Balzer <balzer@expeedo.de>
2021-09-28 11:22:25 +0200

    TLS: add new Let's Encrypt ISRG Root X1 certificate,   add user info on expiration to documentation

--- vehicle/OVMS.V3/components/ovms_tls/src/ovms_tls.cpp
+++ vehicle/OVMS.V3/components/ovms_tls/src/ovms_tls.cpp
@@ -155,6 +155,9 @@ void OvmsTLS::Reload()
   extern const char dst[] asm("_binary_dst_crt_start");
   m_trustlist["DST Root CA X3"] = new OvmsTrustedCert((char*)dst, false);
 
+  extern const char isrg_x1[] asm("_binary_isrg_x1_crt_start");
+  m_trustlist["ISRG Root X1"] = new OvmsTrustedCert((char*)isrg_x1, false);
+
   extern const char digicert_global[] asm("_binary_digicert_global_crt_start");
   m_trustlist["DigiCert Global Root CA"] = new OvmsTrustedCert((char*)digicert_global, false);
 
