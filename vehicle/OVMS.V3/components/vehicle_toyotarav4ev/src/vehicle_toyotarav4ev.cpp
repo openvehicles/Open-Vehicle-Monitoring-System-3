880b17d4055a378e18b95f316f3c8b89725e5121
mikeiimura <rav4ev1462@gmail.com>
2022-04-16 16:54:18 -0700

    JdeMO DC Charging Detection
    Added DC charging detection and charge modes, ideal range calc, gear letter metric, odometer.

--- vehicle/OVMS.V3/components/vehicle_toyotarav4ev/src/vehicle_toyotarav4ev.cpp
+++ vehicle/OVMS.V3/components/vehicle_toyotarav4ev/src/vehicle_toyotarav4ev.cpp
@@ -6,6 +6,9 @@
 ;    0.1.0  Trial code
 ;    0.2.0  Code initially submitted to master branch
 ;    0.3.0  Implemented v_on and v_charging states with some metrics only set/cleared depending on these states
+;    0.4.0  Calc Ideal Range from Avail kWh, Changed v_on to be gated on BMS_status instead of BMS_CtrSet to 
+;           eliminate the overlap with AC charging state. Implemented DC charging detection and v_dc_charging.
+;           Dropped SOC_ui. Added gear letter metric. Added odometer decode and display.
 ;
 ;    (C) 2021-2022 Mike Iimura
 ;
@@ -55,15 +58,19 @@ OvmsVehicleToyotaRav4Ev::OvmsVehicleToyotaRav4Ev()
   memset(m_type,0,sizeof(m_type));
   v_on = false;
   v_charging = false;
+  v_dc_charging = false;
   iFrameCountCan1 = 0;
   iFrameCountCan2 = 0;
+  iNegPowerCount = 0;
   // m_charge_w = 0;
 
+  // m_v_bat_socui = MyMetrics.InitFloat("xr4.v.b.socui", SM_STALE_MIN, 0, Percentage);
   m_v_bat_cool_in_temp = MyMetrics.InitFloat("xr4.v.b.t.cltin", SM_STALE_MIN, 0, Celcius);
   m_v_bat_cool_out_temp = MyMetrics.InitFloat("xr4.v.b.t.cltout", SM_STALE_MIN, 0, Celcius);
   m_v_bat_cool_pump_1  = MyMetrics.InitInt("xr4.v.b.p.one", SM_STALE_HIGH, 0, Percentage);
   m_v_bat_cool_pump_2  = MyMetrics.InitInt("xr4.v.b.p.two", SM_STALE_HIGH, 0, Percentage);
   m_v_bat_energy_avail = MyMetrics.InitFloat("xr4.v.b.en.avail", SM_STALE_MIN, 0, kWh);
+  m_v_env_gear_letter = MyMetrics.InitString("xr4.v.e.gear.letter", SM_STALE_NONE, 0);
   m_v_mot_cool_in_temp = MyMetrics.InitFloat("xr4.v.m.t.cltin", SM_STALE_MIN, 0, Celcius);
   // m_v_mot_cool_out_temp = MyMetrics.InitFloat("xr4.v.m.t.cltout", SM_STALE_MIN, 0, Celcius);
   // it turns out this is value is not populated by the RAV4 Thermal Control ECU, it came from a Model S DBC
@@ -101,26 +108,61 @@ OvmsVehicleToyotaRav4Ev::~OvmsVehicleToyotaRav4Ev()
 
 void OvmsVehicleToyotaRav4Ev::Ticker1(uint32_t ticker)
   {
-  if ((m_candata_timer > 0)&&(--m_candata_timer == 0))
+    if ((m_candata_timer > 0)&&(--m_candata_timer == 0))
     {
     // Car has gone to sleep
-    ESP_LOGI(TAG,"Car has gone to sleep (CAN bus timeout)");
-    StandardMetrics.ms_v_env_awake->SetValue(false);
+      ESP_LOGI(TAG,"Car has gone to sleep (CAN bus timeout)");
+      StandardMetrics.ms_v_env_awake->SetValue(false);
     }
-/*
-  if (StandardMetrics.ms_v_charge_12v_current->IsStale())
-    {
-    StandardMetrics.ms_v_bat_12v_current->SetValue(float(0));
-    }
-*/
+    if (v_on)
+    {     // check for negative pack power while in Park - must be DC charging.
+      if ((StandardMetrics.ms_v_bat_power->AsFloat() < 0) && (m_v_env_gear_letter->AsString()=="P") && (StandardMetrics.ms_v_charge_pilot->AsBool()==false))
+      {
+        iNegPowerCount++;
+        if (!v_dc_charging && iNegPowerCount > 2)
+        {   // set DC Charging state
+          StdMetrics.ms_v_door_chargeport->SetValue(true);
+          StandardMetrics.ms_v_charge_state->SetValue("charging");
+          StandardMetrics.ms_v_charge_substate->SetValue("onrequest");
+          StandardMetrics.ms_v_charge_type->SetValue("chademo");
+          StandardMetrics.ms_v_charge_mode->SetValue("standard");
+          StandardMetrics.ms_v_charge_inprogress->SetValue(true);
+          v_dc_charging = true;
+          ESP_LOGD(TAG,"DC Charging Started");
+        }
+      } else {
+        if (StandardMetrics.ms_v_bat_power->AsFloat() > 0) 
+        {
+          iNegPowerCount = 0;
+          if ((StandardMetrics.ms_v_env_gear->AsInt()==0) && v_dc_charging)
+          {   // clear DC charging state
+            StdMetrics.ms_v_door_chargeport->SetValue(false);
+            StandardMetrics.ms_v_charge_state->SetValue("stopped");
+            StandardMetrics.ms_v_charge_substate->SetValue("stopped");
+            StandardMetrics.ms_v_charge_type->SetValue("");
+            StandardMetrics.ms_v_charge_mode->SetValue("");
+            StandardMetrics.ms_v_charge_inprogress->SetValue(false);
+            v_dc_charging = false;
+            ESP_LOGD(TAG,"DC Charging Stopped");
+          }
+        }
+      }
+    } 
     ESP_LOGV(TAG,"CAN1 frames: %d", iFrameCountCan1);   // Verbose logging of frames per second on each bus
     ESP_LOGV(TAG,"CAN2 frames: %d", iFrameCountCan2);
     iFrameCountCan1=0;
     iFrameCountCan2=0;
   }
+
 void OvmsVehicleToyotaRav4Ev::Ticker60(uint32_t ticker)
 {
-  StandardMetrics.ms_v_bat_temp->SetValue(StandardMetrics.ms_v_bat_cell_tmax->AsFloat());
+  //  StandardMetrics.ms_v_bat_temp->SetValue(StandardMetrics.ms_v_bat_cell_tmax->AsFloat());   // moved to direct decode of 0x312
+  if (StandardMetrics.ms_v_env_on->AsBool())
+  {
+//    ESP_LOGD(TAG,"SOC_MIN: %.1f, SOC_UI: %.1f, Pack_NRG: %.1f", StandardMetrics.ms_v_bat_soc->AsFloat(), m_v_bat_socui->AsFloat(), m_v_bat_energy_avail->AsFloat());
+    ESP_LOGD(TAG,"SOC_MIN: %.1f, Pack_NRG: %.1f", StandardMetrics.ms_v_bat_soc->AsFloat(), m_v_bat_energy_avail->AsFloat());
+  }
+    
 }
 
 // IncomingFrameCan1 (Tesla)
@@ -159,28 +201,36 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan1(CAN_frame_t* p_frame)
         {
         case 1: // Park
           StandardMetrics.ms_v_env_gear->SetValue(0);
+          m_v_env_gear_letter->SetValue("P");
 //          StandardMetrics.ms_v_env_on->SetValue(false);
           StandardMetrics.ms_v_env_handbrake->SetValue(true);
-          StandardMetrics.ms_v_env_charging12v->SetValue(false);
+//          StandardMetrics.ms_v_env_charging12v->SetValue(false);
           break;
         case 2: // Reverse
           StandardMetrics.ms_v_env_gear->SetValue(-1);
+          m_v_env_gear_letter->SetValue("R");
 //          StandardMetrics.ms_v_env_on->SetValue(true);
           StandardMetrics.ms_v_env_handbrake->SetValue(false);
-          StandardMetrics.ms_v_env_charging12v->SetValue(true);
+//          StandardMetrics.ms_v_env_charging12v->SetValue(true);
           break;
         case 3: // Neutral
           StandardMetrics.ms_v_env_gear->SetValue(0);
+          m_v_env_gear_letter->SetValue("N");
 //          StandardMetrics.ms_v_env_on->SetValue(false);
           StandardMetrics.ms_v_env_handbrake->SetValue(false);
-          StandardMetrics.ms_v_env_charging12v->SetValue(true);
+//          StandardMetrics.ms_v_env_charging12v->SetValue(true);
           break;
         case 4: // Drive
+          StandardMetrics.ms_v_env_gear->SetValue(1);
+          m_v_env_gear_letter->SetValue("D");
+          StandardMetrics.ms_v_env_handbrake->SetValue(false);
+          break;
         case 5: // B Mode
           StandardMetrics.ms_v_env_gear->SetValue(1);
+          m_v_env_gear_letter->SetValue("B");
 //          StandardMetrics.ms_v_env_on->SetValue(true);
           StandardMetrics.ms_v_env_handbrake->SetValue(false);
-          StandardMetrics.ms_v_env_charging12v->SetValue(true);
+//          StandardMetrics.ms_v_env_charging12v->SetValue(true);
           break;
         default:
           break;
@@ -210,8 +260,9 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan1(CAN_frame_t* p_frame)
       }  
     case 0x212:   // BMS_status
       {
-        if (d[2] & BMS_CTRSET_CLOSED)   // Contactor is closed, passing HVDC to the car. This is a proxy for READY status.
-        {
+//        if (d[2] & BMS_CTRSET_CLOSED)   // Contactor is closed, passing HVDC to the car. This is a proxy for READY status.
+        if ((d[2]>>4) == BMS_DRIVE)      // BMS indicates DRIVE status. This is a proxy for vehicle READY status.
+        {                               // BMS status is BMS_CHARGER when contactors are closed for AC charging.
           if (!v_on)
           {
             StandardMetrics.ms_v_env_on->SetValue(true);
@@ -242,9 +293,11 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan1(CAN_frame_t* p_frame)
           case 0x00:  // Standby
             if (v_charging)
             {
-              StdMetrics.ms_v_door_chargeport->SetValue(false);
+              StandardMetrics.ms_v_door_chargeport->SetValue(false);
               StandardMetrics.ms_v_charge_state->SetValue("stopped");
               StandardMetrics.ms_v_charge_substate->SetValue("stopped");
+              StandardMetrics.ms_v_charge_type->SetValue("");
+              StandardMetrics.ms_v_charge_mode->SetValue("");
               StandardMetrics.ms_v_charge_inprogress->SetValue(false);
               v_charging = false;
               ESP_LOGD(TAG,"Charging Stopped");
@@ -263,9 +316,11 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan1(CAN_frame_t* p_frame)
           case 0x04:  // Enable
             if (!v_charging)
             {
-              StdMetrics.ms_v_door_chargeport->SetValue(true);
+              StandardMetrics.ms_v_door_chargeport->SetValue(true);
               StandardMetrics.ms_v_charge_state->SetValue("charging");
               StandardMetrics.ms_v_charge_substate->SetValue("onrequest");
+              StandardMetrics.ms_v_charge_type->SetValue("type1");
+              StandardMetrics.ms_v_charge_mode->SetValue("standard");
               StandardMetrics.ms_v_charge_inprogress->SetValue(true);
               v_charging = true;
               ESP_LOGD(TAG,"Charging Started");
@@ -299,10 +354,10 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan1(CAN_frame_t* p_frame)
 
     case 0x302: // BMS_socStatus
       {
-      // BMS_socUI from Tesla Model S
-      //  StandardMetrics.ms_v_bat_soc->SetValue( (((int)d[1]>>2) + (((int)d[2] & 0x0f)<<6))/10 );
+//        m_v_bat_socui->SetValue( float((uint16_t(d[1]>>2)) + ((uint16_t(d[2] & 0x0f))<<6))/10 );           // BMS_socUI
         StandardMetrics.ms_v_bat_soc->SetValue( ((((int)d[1] & 0x03)<<8) + d[0])/10 );      // BMS_socMin 
         m_v_bat_energy_avail->SetValue( float((((int)d[4]&0x0F)<<6)+((d[3]&0xFC)>>2))/10 ); // BMS_socNrg
+        StandardMetrics.ms_v_bat_range_ideal->SetValue(m_v_bat_energy_avail->AsFloat()*5.635);  // Ideal Range = avail kWh * 5.635km/kWh 
       break;
       }
     case 0x306: // DI_temperature
@@ -310,6 +365,11 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan1(CAN_frame_t* p_frame)
         StandardMetrics.ms_v_inv_temp->SetValue((int)d[1]-40);
         StandardMetrics.ms_v_mot_temp->SetValue((int)d[2]-40);
       break;
+      }
+    case 0x312: // BMS_thermalStatus
+      {
+        StandardMetrics.ms_v_bat_temp->SetValue(((int8_t)d[6])+40);
+        break;
       }
     case 0x31A: // THC_coolantTemp
       {
@@ -332,6 +392,11 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan1(CAN_frame_t* p_frame)
         uint32_t iChgCntr = (d[7]<<24)+(d[6]<<16)+(d[5]<<8)+d[4];
         StandardMetrics.ms_v_bat_energy_used_total->SetValue(float(iDischCntr)/1000);
         StandardMetrics.ms_v_charge_kwh_grid_total->SetValue(float(iChgCntr)/1000);
+        break;
+      }
+    case 0x562: // BMS_OdometerStatus
+      {
+        StandardMetrics.ms_v_pos_odometer->SetValue(float((d[3]<<24)+(d[2]<<16)+(d[1]<<8)+d[0])*0.001, Miles);
         break;
       }
     case 0x6f2: // BMS brick voltage and module temperatures
@@ -376,10 +441,12 @@ void OvmsVehicleToyotaRav4Ev::IncomingFrameCan2(CAN_frame_t* p_frame)
 
   }
 
+/*
 void OvmsVehicleToyotaRav4Ev::IncomingFrameCan3(CAN_frame_t* p_frame)
   {
   // Toyota RAV4 EV CAN3 Not Used
   }
+*/
 
 void OvmsVehicleToyotaRav4Ev::Notify12vCritical()
   { // Not supported on RAV4 EV
@@ -405,3 +472,74 @@ OvmsVehicleToyotaRav4EvInit::OvmsVehicleToyotaRav4EvInit()
 
   MyVehicleFactory.RegisterVehicle<OvmsVehicleToyotaRav4Ev>("TYR4","Toyota RAV4 EV");
   }
+
+/**
+ * GetDashboardConfig: RAV4 EV specific dashboard setup
+ 
+void OvmsVehicleToyotaRav4Ev::GetDashboardConfig(DashboardConfig& cfg)
+{
+  cfg.gaugeset1 =
+    "yAxis: [{"
+      // Speed:
+      "min: 0, max: 120,"
+      "plotBands: ["
+        "{ from: 0, to: 70, className: 'green-band' },"
+        "{ from: 70, to: 100, className: 'yellow-band' },"
+        "{ from: 100, to: 120, className: 'red-band' }]"
+    "},{"
+      // Voltage:
+      "min: 45, max: 60,"
+      "plotBands: ["
+        "{ from: 45, to: 47.5, className: 'red-band' },"
+        "{ from: 47.5, to: 50, className: 'yellow-band' },"
+        "{ from: 50, to: 60, className: 'green-band' }]"
+    "},{"
+      // SOC:
+      "min: 0, max: 100,"
+      "plotBands: ["
+        "{ from: 0, to: 12.5, className: 'red-band' },"
+        "{ from: 12.5, to: 25, className: 'yellow-band' },"
+        "{ from: 25, to: 100, className: 'green-band' }]"
+    "},{"
+      // Efficiency:
+      "min: 0, max: 300,"
+      "plotBands: ["
+        "{ from: 0, to: 150, className: 'green-band' },"
+        "{ from: 150, to: 250, className: 'yellow-band' },"
+        "{ from: 250, to: 300, className: 'red-band' }]"
+    "},{"
+      // Power:
+      "min: -10, max: 30,"
+      "plotBands: ["
+        "{ from: -10, to: 0, className: 'violet-band' },"
+        "{ from: 0, to: 15, className: 'green-band' },"
+        "{ from: 15, to: 25, className: 'yellow-band' },"
+        "{ from: 25, to: 30, className: 'red-band' }]"
+    "},{"
+      // Charger temperature:
+      "min: 20, max: 80, tickInterval: 20,"
+      "plotBands: ["
+        "{ from: 20, to: 65, className: 'normal-band border' },"
+        "{ from: 65, to: 80, className: 'red-band border' }]"
+    "},{"
+      // Battery temperature:
+      "min: -15, max: 65, tickInterval: 25,"
+      "plotBands: ["
+        "{ from: -15, to: 0, className: 'red-band border' },"
+        "{ from: 0, to: 50, className: 'normal-band border' },"
+        "{ from: 50, to: 65, className: 'red-band border' }]"
+    "},{"
+      // Inverter temperature:
+      "min: 20, max: 80, tickInterval: 20,"
+      "plotBands: ["
+        "{ from: 20, to: 70, className: 'normal-band border' },"
+        "{ from: 70, to: 80, className: 'red-band border' }]"
+    "},{"
+      // Motor temperature:
+      "min: 50, max: 125, tickInterval: 25,"
+      "plotBands: ["
+        "{ from: 50, to: 110, className: 'normal-band border' },"
+        "{ from: 110, to: 125, className: 'red-band border' }]"
+    "}]";
+}
+*/
