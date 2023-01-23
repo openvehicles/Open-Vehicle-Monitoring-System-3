d3821c36a3dd383ffa19fb7393e31de7d5b8896c
Ludovic LANGE <llange@users.noreply.github.com>
2023-01-23 23:56:54 +0100

    ESP-IDF v4+ : introduce "ESP-NETIF" as a replacement for "TCP/IP Adaptor" for ESP-IDF >= 4 (followup)
    Conditional compilation depending on the ESP-IDF version
    
    Signed-off-by: Ludovic LANGE <llange@users.noreply.github.com>

--- vehicle/OVMS.V3/main/ovms_netmanager.h
+++ vehicle/OVMS.V3/main/ovms_netmanager.h
@@ -33,7 +33,11 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#if ESP_IDF_VERSION_MAJOR >= 4
+#include "esp_netif.h"
+#else
 #include "tcpip_adapter.h"
+#endif
 extern "C"
   {
 #include "lwip/netif.h"
