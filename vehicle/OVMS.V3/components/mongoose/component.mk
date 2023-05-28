9d4192a3ed97bb51a65b556d4a55a2bf16b0f4ba
Ludovic LANGE <llange@users.noreply.github.com>
2023-05-29 00:12:02 +0200

    identify dependencies to mongoose, and generate a mongoose version header
    In preparation of a future mongoose upgrade, we need to detect current
    mongoose version.
    This is done by generating a private header `include/mg_version.h` which is
    generated at compile time (both by `component.mk` and `CMakeLists.txt`, from
    a template file `mg_version.h.in` and the version in `mongoose.h`)
    
    To ensure proper compilation order (the generated file needs to be created before
    being included), we need to identify components that depend on the build of mongoose.
    This is recorded both in `component.mk` and `CMakeLists.txt`).
    
    Signed-off-by: Ludovic LANGE <llange@users.noreply.github.com>

--- vehicle/OVMS.V3/components/mongoose/component.mk
+++ vehicle/OVMS.V3/components/mongoose/component.mk
@@ -8,6 +8,25 @@
 #
 
 ifdef CONFIG_OVMS_SC_GPL_MONGOOSE
+COMPONENT_EXTRA_CLEAN := $(COMPONENT_PATH)/include/mg_version.h
+
+mongoose/mongoose.o: $(COMPONENT_PATH)/include/mg_version.h
+
+$(COMPONENT_PATH)/include/mg_version.h: SHELL := /bin/bash
+$(COMPONENT_PATH)/include/mg_version.h: $(COMPONENT_PATH)/mongoose/mongoose.h
+	{ \
+		MG_VERSION=$$(cat "$(COMPONENT_PATH)/mongoose/mongoose.h" | \
+		grep "#define MG_VERSION" | \
+		cut "-d" " " "-f3" | \
+		sed -e s/\"//g); \
+		echo "Compiled Mongoose version: $${MG_VERSION}"; \
+		a=( $${MG_VERSION//./ } ); \
+		cat "$(COMPONENT_PATH)/mg_version.h.in" | \
+			sed -e "s/@MG_VERSION_MAJOR@/$${a[0]}/" \
+					-e "s/@MG_VERSION_MINOR@/$${a[1]}/" \
+					-e "s/@MG_VERSION_PATCH@/0/" \
+			> "$(COMPONENT_PATH)/include/mg_version.h"; \
+	}
 COMPONENT_ADD_INCLUDEDIRS := include mongoose
 ifdef CONFIG_MG_SSL_IF_WOLFSSL
 COMPONENT_PRIV_INCLUDEDIRS := ../wolfssl ../wolfssl/wolfssl
