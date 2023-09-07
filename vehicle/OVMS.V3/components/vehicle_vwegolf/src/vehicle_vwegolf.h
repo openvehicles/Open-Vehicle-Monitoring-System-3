190efd30ae7ff7129d0d225589ad678545eb11a4
Jared Greco <jaredgreco@gmail.com>
2023-09-06 23:07:49 -0700

    stub for VW e-Golf

---
+++ vehicle/OVMS.V3/components/vehicle_vwegolf/src/vehicle_vwegolf.h
@@ -0,0 +1,43 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          14th March 2017
+;
+;    Changes:
+;    1.0  Initial release
+;
+;    (C) 2011       Michael Stegen / Stegen Electronics
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#ifndef __VEHICLE_VWEG_H__
+#define __VEHICLE_VWEG_H__
+
+#include "vehicle.h"
+
+using namespace std;
+
+class OvmsVehicleVWeGolf : public OvmsVehicle
+  {
+  public:
+	OvmsVehicleVWeGolf();
+    ~OvmsVehicleVWeGolf();
+  };
+
+#endif //#ifndef __VEHICLE_VWEG_H__
