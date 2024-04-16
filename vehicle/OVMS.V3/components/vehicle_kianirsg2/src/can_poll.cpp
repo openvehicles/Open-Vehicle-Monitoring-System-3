7ae843b4a7cf6083ac9e0c403834aa6b562e82f6
Jaime <jmiddleton@uc.cl>
2024-04-16 12:21:15 -0400

    niro compiling

---
+++ vehicle/OVMS.V3/components/vehicle_kianirsg2/src/can_poll.cpp
@@ -0,0 +1,281 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          21th January 2019
+;
+;    (C) 2019       Geir Øyvind Vælidalo <geir@validalo.net>
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+#include "vehicle_kia_niroevsg2.h"
+
+static const char *TAG = "v-kianiroevsg2";
+
+/**
+ * Incoming poll reply messages
+ */
+void OvmsVehicleKiaNiroEvSg2::IncomingPollReply(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain)
+{
+	//ESP_LOGD(TAG, "IPR %03x TYPE:%x PID:%02x %x %02x %02x %02x %02x %02x %02x %02x %02x", m_poll_moduleid_low, type, pid, length, data[0], data[1], data[2], data[3],
+	//	data[4], data[5], data[6], data[7]);
+	switch (m_poll_moduleid_low)
+		{
+		// ****** IGMP *****
+		case 0x778:
+			IncomingIGMP(bus, type, pid, data, length, mlremain);
+			break;
+
+		// ****** BCM ******
+		case 0x7a8:
+			IncomingBCM(bus, type, pid, data, length, mlremain);
+			break;
+
+		// ******* VMCU ******
+		case 0x7ea:
+			IncomingVMCU(bus, type, pid, data, length, mlremain);
+			break;
+
+		// ***** BMC ****
+		case 0x7ec:
+			IncomingBMC(bus, type, pid, data, length, mlremain);
+			break;
+
+		// ***** CM ****
+		case 0x7ce:
+			IncomingCM(bus, type, pid, data, length, mlremain);
+			break;
+
+		// ***** SW ****
+		case 0x7dc:
+			IncomingSW(bus, type, pid, data, length, mlremain);
+			break;
+
+		default:
+			ESP_LOGD(TAG, "Unknown module: %03x", m_poll_moduleid_low);
+			break;
+		}
+}
+
+/**
+ * Handle incoming messages from cluster.
+ */
+void OvmsVehicleKiaNiroEvSg2::IncomingCM(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain)
+{
+//	ESP_LOGI(TAG, "CM PID:%02x %x %02x %02x %02x %02x %02x %02x %02x %02x", pid, length, m_poll_ml_frame, data[0], data[1], data[2], data[3],
+//			data[4], data[5], data[6]);
+//	ESP_LOGI(TAG, "---");
+	switch (pid)
+		{
+		case 0xb002:
+			if (m_poll_ml_frame == 1)
+				{
+				StdMetrics.ms_v_pos_odometer->SetValue(CAN_UINT24(3), GetConsoleUnits());
+				}
+				break;
+		case 0xb003:
+				if (m_poll_ml_frame == 1)
+				{
+				StdMetrics.ms_v_env_awake->SetValue(CAN_BYTE(1)!=0);
+				if (!StdMetrics.ms_v_env_awake->AsBool())
+				{
+					StdMetrics.ms_v_env_on->SetValue(false);
+				}
+				
+				}
+				break;
+		}
+}
+
+/**
+ * Handle incoming messages from VMCU-poll
+ *
+ * - Aux battery SOC, Voltage and current
+ */
+void OvmsVehicleKiaNiroEvSg2::IncomingVMCU(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain)
+{
+	//ESP_LOGD(TAG, "VMCU TYPE: %02x PID:%02x %x %02x %02x %02x %02x %02x %02x %02x %02x", type, pid, length, m_poll_ml_frame, data[0], data[1], data[2], data[3],
+	//		data[4], data[5], data[6]);
+
+	switch (pid)
+	{
+	case 0x02:
+		if (type == VEHICLE_POLL_TYPE_OBDIIGROUP)
+			{
+			if (m_poll_ml_frame == 3)
+				{
+				StdMetrics.ms_v_bat_12v_voltage->SetValue(((CAN_BYTE(2)<<8)+CAN_BYTE(1))/1000.0, Volts);
+				}
+			}
+		break;
+	}
+}
+
+/**
+ * Handle incoming messages from BMC-poll
+ *
+ * - Pilot signal available
+ * - CCS / Type2
+ * - Battery current
+ * - Battery voltage
+ * - Battery module temp 1-8
+ * - Cell voltage max / min + cell #
+ * + more
+ */
+void OvmsVehicleKiaNiroEvSg2::IncomingBMC(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain)
+{
+	if (type == VEHICLE_POLL_TYPE_OBDIIEXTENDED)
+	{
+		switch (pid)
+		{
+		case 0x0101:
+			// diag page 01: skip first frame (no data)
+			//ESP_LOGD(TAG, "Frame number %x",m_poll_ml_frame);
+			
+			if (m_poll_ml_frame == 2)
+				{
+				StdMetrics.ms_v_bat_current->SetValue((float)CAN_INT(0)/10.0, Amps);
+				StdMetrics.ms_v_bat_voltage->SetValue((float)CAN_UINT(2)/10.0, Volts);
+				}
+			break;
+
+		case 0x0105:
+			if (m_poll_ml_frame == 4)
+				{
+				StdMetrics.ms_v_bat_soh->SetValue( (float)CAN_UINT(1)/10.0 );
+				}
+			else if (m_poll_ml_frame == 5)
+				{
+				StdMetrics.ms_v_bat_soc->SetValue(CAN_BYTE(0)/2.0);
+				}
+			break;
+		}
+	}
+}
+
+
+
+/**
+ * Handle incoming messages from BCM-poll
+ */
+void OvmsVehicleKiaNiroEvSg2::IncomingBCM(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain)
+{
+	uint8_t bVal;
+	if (type == VEHICLE_POLL_TYPE_OBDIIEXTENDED)
+		{
+		switch (pid)
+			{
+			case 0xC00B:
+				if (m_poll_ml_frame == 1)
+				{
+					bVal = CAN_BYTE(1);
+					if (bVal > 0)
+						StdMetrics.ms_v_tpms_pressure->SetElemValue(MS_V_TPMS_IDX_FL, bVal / 5.0, PSI);
+					bVal = CAN_BYTE(6);
+					if (bVal > 0)
+						StdMetrics.ms_v_tpms_pressure->SetElemValue(MS_V_TPMS_IDX_FR, bVal / 5.0, PSI);
+				}
+				else if (m_poll_ml_frame == 2)
+				{
+					bVal = CAN_BYTE(4);
+					if (bVal > 0)
+						StdMetrics.ms_v_tpms_pressure->SetElemValue(MS_V_TPMS_IDX_RL, bVal / 5.0, PSI);
+				}
+				else if (m_poll_ml_frame == 3)
+				{
+					bVal = CAN_BYTE(2);
+					if (bVal > 0)
+						StdMetrics.ms_v_tpms_pressure->SetElemValue(MS_V_TPMS_IDX_RR, bVal / 5.0, PSI);
+				}
+				break;
+
+			case 0xB010:
+				if (m_poll_ml_frame == 1)
+				{
+					bVal = CAN_BYTE(1);
+					if (bVal > 0){
+						windows_open = true;
+					} else {
+						windows_open = false;
+					}
+				}
+				break;
+			}
+		}
+	}
+
+/**
+ * Handle incoming messages from IGMP-poll
+ *
+ *
+ */
+void OvmsVehicleKiaNiroEvSg2::IncomingIGMP(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain)
+{
+	if (type == VEHICLE_POLL_TYPE_OBDIIEXTENDED)
+	{
+		switch (pid)
+		{
+		case 0xbc03:
+			if (m_poll_ml_frame == 1)
+				{
+				StdMetrics.ms_v_door_fl->SetValue(CAN_BIT(1, 5));
+				StdMetrics.ms_v_door_fr->SetValue(CAN_BIT(1, 4));
+				StdMetrics.ms_v_door_rl->SetValue(CAN_BIT(1, 0));
+				StdMetrics.ms_v_door_rr->SetValue(CAN_BIT(1, 2));
+				m_v_door_lock_rl->SetValue(!CAN_BIT(1, 1));
+				m_v_door_lock_rr->SetValue(!CAN_BIT(1, 3));
+				}
+			break;
+
+		case 0xbc04:
+			if (m_poll_ml_frame == 1)
+				{
+				m_v_door_lock_fl->SetValue(!CAN_BIT(1, 3));
+				m_v_door_lock_fr->SetValue(!CAN_BIT(1, 2));
+				}
+			break;
+		}
+	}
+}
+
+
+void OvmsVehicleKiaNiroEvSg2::IncomingSW(canbus *bus, uint16_t type, uint16_t pid, uint8_t *data, uint8_t length, uint16_t mlremain)
+{
+	if (type == VEHICLE_POLL_TYPE_OBDIIEXTENDED)
+	{
+		switch (pid)
+		{
+		case 0x0101:
+			if (m_poll_ml_frame == 2)
+			{
+				StdMetrics.ms_v_env_on->SetValue(CAN_BYTE(6) != 0);
+				StdMetrics.ms_v_pos_speed->SetValue(CAN_BYTE(2));
+			}
+			break;
+		}
+	}
+}
+
+/**
+ * Get console ODO units
+ *
+ * Currently from configuration
+ */
+metric_unit_t OvmsVehicleKiaNiroEvSg2::GetConsoleUnits()
+{
+	return MyConfig.GetParamValueBool("xkn", "consoleKilometers", true) ? Kilometers : Miles;
+}
---
+++ vehicle/OVMS.V3/components/vehicle_kianirsg2/src/vehicle_kia_niroevsg2.cpp
@@ -0,0 +1,384 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          21th January 2019
+;
+;    Changes:
+;    0.0.1  Initial stub
+;			- Ported from Kia Soul. Totally untested.
+;
+;    0.1.0  First version on par with Soul
+;			- First "complete" version.
+;
+;		0.1.1 06-apr-2019 - Geir Øyvind Vælidalo
+;			- Minor changes after proper real life testing
+;			- VIN is working
+;			- Removed more of the polling when car is off in order to prevent AUX battery drain
+;
+;		0.1.2 10-apr-2019 - Geir Øyvind Vælidalo
+;			- Fixed TPMS reading
+;			- Fixed xks aux
+;			- Estimated range show WLTP in lack of the actual displayed range
+;			- Door lock works even after ECU goes to sleep.
+;
+;		0.1.3 12-apr-2019 - Geir Øyvind Vælidalo
+;			- Fixed OBC temp reading
+;			- Removed a couple of pollings when car is off, in order to save AUX battery
+;			- Added range calculator for estimated range instead of WLTP. It now uses 20 last trips as a basis.
+;
+;		0.1.5 18-apr-2019 - Geir Øyvind Vælidalo
+;			- Changed poll frequencies to minimize the strain on the CAN-write function.
+;
+;		0.1.6 20-apr-2019 - Geir Øyvind Vælidalo
+;			- AUX Battery monitor..
+;
+;    (C) 2011       Michael Stegen / Stegen Electronics
+;    (C) 2011-2017  Mark Webb-Johnson
+;    (C) 2011       Sonny Chen @ EPRO/DX
+;    (C) 2019       Geir Øyvind Vælidalo <geir@validalo.net>
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#include "vehicle_kia_niroevsg2.h"
+
+#include "ovms_log.h"
+#include <stdio.h>
+#include <string.h>
+#include "pcp.h"
+#include "metrics_standard.h"
+#include "ovms_metrics.h"
+#include <sys/param.h>
+#include "common.h"
+#include "ovms_boot.h"
+#include "ovms_events.h"
+
+#define VERSION "0.1.6"
+
+static const char *TAG = "v-kianiroevsg2";
+
+// Pollstate 0 - car is off
+// Pollstate 1 - car is on
+// Pollstate 2 - car is charging
+static const OvmsVehicle::poll_pid_t vehicle_kianiroevsg2_polls[] =
+	{
+		// ok2	IncomingIGMP
+		{0x770, 0x778, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0xbc03, {2, 2, 2}, 1, ISOTP_STD}, // IGMP Door status and lock
+		{0x770, 0x778, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0xbc04, {2, 2, 2}, 1, ISOTP_STD}, // IGMP Door lock
+
+		// ok2 IncomingBCM
+		{0x7a0, 0x7a8, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0xC00B, {0, 25, 0}, 1, ISOTP_STD}, // TMPS - Pressure
+		{0x7a0, 0x7a8, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0xB010, {0, 25, 0}, 1, ISOTP_STD}, // TMPS - Pressure
+
+		// ok2	IncomingVMCU
+		{0x7e2, 0x7ea, VEHICLE_POLL_TYPE_OBDIIGROUP, 0x02, {600, 25, 25}, 1, ISOTP_STD}, // VMCU - Aux Battery voltage
+
+		// ok2	IncomingBMC
+		{0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x0101, {5, 2, 2}, 1, ISOTP_STD}, // Voltage and current main bat
+		{0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x0105, {0, 5, 5}, 1, ISOTP_STD}, // bat soc and soh
+
+		// ok2	IncomingCM
+		{0x7c6, 0x7ce, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0xB002, {0, 25, 60}, 1, ISOTP_STD}, // ODO
+		{0x7c6, 0x7ce, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0xB003, {2, 2, 2}, 1, ISOTP_STD},	// AWAKE
+
+		// ok2	IncomingSteeringwheel
+		{0x7d4, 0x7dc, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x0101, {2, 2, 2}, 1, ISOTP_STD}, // ON SPEED
+		POLL_LIST_END};
+
+static const OvmsVehicle::poll_pid_t vehicle_kianiroevsg2_polls_stop[] =
+	{
+		POLL_LIST_END};
+
+/**
+ * Constructor for Kia Niro EV OvmsVehicleKiaNiroEvSg2
+ */
+OvmsVehicleKiaNiroEvSg2::OvmsVehicleKiaNiroEvSg2()
+{
+	ESP_LOGI(TAG, "Kia Niro Sg2 v1.0 vehicle module");
+
+	message_send_can.id = 0;
+	message_send_can.status = 0;
+	can_2_sending = true;
+	should_poll = true;
+
+	memset(message_send_can.byte, 0, sizeof(message_send_can.byte));
+	windows_open = false;
+	start_alarm = false;
+	lock_command = false;
+	unlock_command = false;
+	fully_configured = false;
+	reset_by_config = false;
+	// SetParamValue
+	// SetParamValueBinary
+	// SetParamValueInt
+	// SetParamValueFloat
+	// SetParamValueBool
+	// GetParamValue\(.*, .*, ".*"\);
+
+	// init metrics:
+	m_v_door_lock_fl = MyMetrics.InitBool("xkn.v.door.lock.front.left", 10, 0);
+	m_v_door_lock_fr = MyMetrics.InitBool("xkn.v.door.lock.front.right", 10, 0);
+	m_v_door_lock_rl = MyMetrics.InitBool("xkn.v.door.lock.rear.left", 10, 0);
+	m_v_door_lock_rr = MyMetrics.InitBool("xkn.v.door.lock.rear.right", 10, 0);
+	m_v_polling = MyMetrics.InitBool("xkn.v.polling", 10, 0);
+
+	StdMetrics.ms_v_bat_12v_voltage->SetValue(12.5, Volts);
+	StdMetrics.ms_v_charge_inprogress->SetValue(false);
+	StdMetrics.ms_v_env_on->SetValue(false);
+	StdMetrics.ms_v_bat_temp->SetValue(20, Celcius);
+
+	// Require GPS.
+	MyEvents.SignalEvent("vehicle.require.gps", NULL);
+	MyEvents.SignalEvent("vehicle.require.gpstime", NULL);
+
+	using std::placeholders::_1;
+	using std::placeholders::_2;
+
+	// #ifdef CONFIG_OVMS_COMP_WEBSERVER
+	// 	WebInit();
+	// #endif
+	// D-Bus
+	PollSetThrottling(6);
+	RegisterCanBus(1, CAN_MODE_ACTIVE, CAN_SPEED_500KBPS);
+	RegisterCanBus(2, CAN_MODE_ACTIVE, CAN_SPEED_500KBPS);
+	POLLSTATE_OFF;
+	PollSetPidList(m_can1, vehicle_kianiroevsg2_polls);
+	m_v_polling->SetValue(true);
+}
+
+/**
+ * Destructor
+ */
+OvmsVehicleKiaNiroEvSg2::~OvmsVehicleKiaNiroEvSg2()
+{
+	ESP_LOGI(TAG, "Shutdown Kia Sg2 EV vehicle module");
+	// MyWebServer.DeregisterPage("/bms/cellmon");
+}
+
+/**
+ * Takes care of setting all the state appropriate when the car is on
+ * or off. Centralized so we can more easily make on and off mirror
+ * images.
+ */
+void OvmsVehicleKiaNiroEvSg2::HandleCarOn()
+{
+	POLLSTATE_RUNNING;
+	ESP_LOGI(TAG, "CAR IS ON | POLLSTATE RUNNING");
+}
+void OvmsVehicleKiaNiroEvSg2::HandleCarOff()
+{
+	POLLSTATE_OFF;
+	ESP_LOGI(TAG, "CAR IS OFF | POLLSTATE OFF");
+	StdMetrics.ms_v_pos_speed->SetValue(0);
+}
+void OvmsVehicleKiaNiroEvSg2::HandleCharging()
+{
+	POLLSTATE_CHARGING;
+	ESP_LOGI(TAG, "CAR IS CHARGING | POLLSTATE RUNNING");
+}
+
+/**
+ * Ticker1: Called every second
+ */
+void OvmsVehicleKiaNiroEvSg2::Ticker1(uint32_t ticker)
+{
+	VerifyCanActivity();
+	can_2_sending = false;
+
+	// Register car as locked only if all doors are locked
+	StdMetrics.ms_v_env_locked->SetValue(
+		m_v_door_lock_fr->AsBool() &
+		m_v_door_lock_fl->AsBool() &
+		m_v_door_lock_rr->AsBool() &
+		m_v_door_lock_rl->AsBool());
+	StdMetrics.ms_v_bat_power->SetValue(
+		StdMetrics.ms_v_bat_voltage->AsFloat(400, Volts) *
+			StdMetrics.ms_v_bat_current->AsFloat(1, Amps) / 1000,
+		kW);
+	StdMetrics.ms_v_charge_inprogress->SetValue(
+		(StdMetrics.ms_v_pos_speed->AsFloat(0) < 1) &
+		(StdMetrics.ms_v_bat_power->AsFloat(0, kW) < -1));
+
+	if (m_poll_state == 0)
+	{
+		// ESP_LOGI(TAG, "POLL STATE OFF");
+	}
+	else if (m_poll_state == 1)
+	{
+		// ESP_LOGI(TAG, "POLL STATE RUNNING");
+	}
+	else if (m_poll_state == 2)
+	{
+		// ESP_LOGI(TAG, "POLL STATE CHARGING");
+		if (!StdMetrics.ms_v_charge_inprogress->AsBool())
+		{
+			HandleChargeStop();
+		}
+		else
+		{
+			SetChargeMetrics();
+		}
+	}
+
+	if (m_poll_state != 0 && !StdMetrics.ms_v_env_on->AsBool() && !StdMetrics.ms_v_charge_inprogress->AsBool())
+	{
+		HandleCarOff();
+	}
+	else if (m_poll_state != 1 && StdMetrics.ms_v_env_on->AsBool() && !StdMetrics.ms_v_charge_inprogress->AsBool())
+	{
+		HandleCarOn();
+	}
+	else if (m_poll_state != 2 && StdMetrics.ms_v_charge_inprogress->AsBool())
+	{
+		HandleCharging();
+	}
+	VerifyDoorLock();
+}
+
+/**
+ * Ticker10: Called every ten seconds
+ */
+void OvmsVehicleKiaNiroEvSg2::Ticker10(uint32_t ticker)
+{
+}
+
+/**
+ * Ticker300: Called every five minutes
+ */
+void OvmsVehicleKiaNiroEvSg2::Ticker300(uint32_t ticker)
+{
+	// check ecus every 20 min even if car is off
+	if (!can_2_sending && poll_counter > 1500)
+	{
+		can_2_sending = true;
+		VerifyCanActivity();
+	}
+}
+
+/**
+ * Update metrics when charging stops
+ */
+void OvmsVehicleKiaNiroEvSg2::HandleChargeStop()
+{
+	ESP_LOGI(TAG, "Charging done...");
+	StdMetrics.ms_v_charge_type->SetValue("");
+}
+
+/**
+ *  Sets the charge metrics
+ */
+void OvmsVehicleKiaNiroEvSg2::SetChargeMetrics()
+{
+	bool ccs = StdMetrics.ms_v_bat_power->AsFloat(0, kW) < -15;
+	StdMetrics.ms_v_charge_type->SetValue(ccs ? "ccs" : "type2");
+}
+
+/**
+ * Open or lock the doors
+ */
+bool OvmsVehicleKiaNiroEvSg2::SetDoorLock(bool lock)
+{
+	if (lock_command || unlock_command)
+	{
+		return false;
+	}
+	SendCanMessageSecondary(0x500, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF);
+	vTaskDelay(pdMS_TO_TICKS(14));
+	SendCanMessageSecondary(0x502, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF);
+	vTaskDelay(pdMS_TO_TICKS(120));
+	SendCanMessageSecondary(0x401, 0x6D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+	vTaskDelay(pdMS_TO_TICKS(2));
+	SendCanMessageSecondary(0x402, 0xC2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+	vTaskDelay(pdMS_TO_TICKS(2));
+	SendCanMessageSecondary(0x403, 0x06, 0x10, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00);
+	vTaskDelay(pdMS_TO_TICKS(2));
+	SendCanMessageSecondary(0x4D6, 0xDD, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+	bool closed_doors = StdMetrics.ms_v_door_fl->AsBool() &&
+						StdMetrics.ms_v_door_fr->AsBool() &&
+						StdMetrics.ms_v_door_rl->AsBool() &&
+						StdMetrics.ms_v_door_rr->AsBool();
+	if (lock)
+	{
+		if (closed_doors)
+		{
+			vTaskDelay(pdMS_TO_TICKS(50));
+			SendCanMessageSecondary(0x4A2, 0x00, 0x00, 0xFC, 0xC0, 0xFF, 0xFF, 0x7F, 0x00);
+			vTaskDelay(pdMS_TO_TICKS(5));
+			SendCanMessageSecondary(0x4A2, 0x00, 0x00, 0xFC, 0xC0, 0xFF, 0xFF, 0x7F, 0x00);
+			vTaskDelay(pdMS_TO_TICKS(28));
+			SendCanMessageSecondary(0x4A2, 0x00, 0x00, 0xFC, 0xC0, 0xFF, 0xFF, 0x7F, 0x00);
+
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		vTaskDelay(pdMS_TO_TICKS(50));
+		SendCanMessageSecondary(0x4A2, 0x00, 0x00, 0xFC, 0xC0, 0xFF, 0xFF, 0xDF, 0x00);
+		vTaskDelay(pdMS_TO_TICKS(5));
+		SendCanMessageSecondary(0x4A2, 0x00, 0x00, 0xFC, 0xC0, 0xFF, 0xFF, 0xDF, 0x00);
+		vTaskDelay(pdMS_TO_TICKS(28));
+		SendCanMessageSecondary(0x4A2, 0x00, 0x00, 0xFC, 0xC0, 0xFF, 0xFF, 0xDF, 0x00);
+	}
+	return true;
+}
+
+void OvmsVehicleKiaNiroEvSg2::VerifyCanActivity()
+{
+	poll_counter++;
+	if (can_2_sending)
+	{
+		if (!should_poll)
+		{
+			should_poll = true;
+			ESP_LOGI(TAG, "Re-enable polling...");
+			PollSetPidList(m_can1, vehicle_kianiroevsg2_polls);
+			m_v_polling->SetValue(true);
+		}
+		poll_counter = 0;
+	}
+	else
+	{
+		if (should_poll)
+		{
+			if (poll_counter > 300)
+			{
+				should_poll = false;
+				ESP_LOGI(TAG, "Disable polling...");
+				PollSetPidList(m_can1, vehicle_kianiroevsg2_polls_stop);
+				m_v_polling->SetValue(false);
+				poll_counter = 0;
+			}
+		}
+	}
+}
+
+class OvmsVehicleKiaNiroEvSg2Init
+{
+public:
+	OvmsVehicleKiaNiroEvSg2Init();
+} MyOvmsVehicleKiaNiroEvSg2Init __attribute__((init_priority(9000)));
+
+OvmsVehicleKiaNiroEvSg2Init::OvmsVehicleKiaNiroEvSg2Init()
+{
+	ESP_LOGI(TAG, "Registering Vehicle: Kia Niro Sg2 EV (9000)");
+	MyVehicleFactory.RegisterVehicle<OvmsVehicleKiaNiroEvSg2>("KN2", "Kia Niro Sg2 EV");
+}
