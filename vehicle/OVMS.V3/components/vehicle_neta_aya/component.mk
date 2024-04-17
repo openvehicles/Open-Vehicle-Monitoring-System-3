2c7a87f58f3c63811916e2ef0e3fd328a6347953
Jaime <jmiddleton@uc.cl>
2024-04-17 16:43:59 -0400

    update sdkconfig

--- vehicle/OVMS.V3/components/vehicle_neta_aya/component.mk
+++ vehicle/OVMS.V3/components/vehicle_neta_aya/component.mk
@@ -7,7 +7,7 @@
 # please read the ESP-IDF documents if you need to do this.
 #
 
-ifdef CONFIG_OVMS_VEHICLE_NETAAYA
+ifdef CONFIG_OVMS_VEHICLE_NETA_AYA
 COMPONENT_ADD_INCLUDEDIRS:=src
 COMPONENT_SRCDIRS:=src
 COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
