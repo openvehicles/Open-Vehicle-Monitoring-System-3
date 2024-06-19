9f38ee9a2aa945e6cdd94e620af0756eaed1221d
Matthieu Labas <matthieu.labas@deepgray.fr>
2024-06-19 14:13:30 +0200

    Added alpha support for Energica motorbikes (SS9, Eva Ribelle and EGO).

---
+++ vehicle/OVMS.V3/components/vehicle_energica/src/vehicle_energica.cpp
@@ -0,0 +1,184 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          14th March 2017
+;
+;    Changes:
+;    1.0  Initial release
+;
+;    (C) 2011       Michael Stegen / Stegen Electronics
+;    (C) 2011-2018  Mark Webb-Johnson
+;    (C) 2011       Sonny Chen @ EPRO/DX
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+/*
+; CREDIT
+; Thanks to Scott451 for figuring out many of the Roadster CAN bus messages used by the OVMS,
+; and the pinout of the CAN bus socket in the Roadster.
+; http://www.teslamotorsclub.com/showthread.php/4388-iPhone-app?p=49456&viewfull=1#post49456"]iPhone app
+; Thanks to fuzzylogic for further analysis and messages such as door status, unlock/lock, speed, VIN, etc.
+; Thanks to markwj for further analysis and messages such as Trip, Odometer, TPMS, etc.
+*/
+
+#include "ovms_log.h"
+static const char *TAG = "v-energica";
+
+#include <stdio.h>
+#include <string.h>
+#include "vehicle_energica.h"
+#include "ovms_metrics.h"
+#include "metrics_standard.h"
+#include "vehicle_energica_pids.h"
+
+// TODO: Detect charge start/stop (PID 0x102 bit 9) and build charge stats
+
+OvmsVehicleEnergica::OvmsVehicleEnergica()
+  {
+  ESP_LOGI(TAG, "Energica vehicle module");
+
+  RegisterCanBus(1,CAN_MODE_ACTIVE,CAN_SPEED_500KBPS);
+  }
+
+OvmsVehicleEnergica::~OvmsVehicleEnergica()
+  {
+  ESP_LOGI(TAG, "Shutdown Energica vehicle module");
+  }
+
+void OvmsVehicleEnergica::IncomingFrameCan1(CAN_frame_t* p_frame)
+{
+	uint8_t *d = p_frame->data.u8;
+
+	switch (p_frame->MsgID) {
+		// Motor temperatures
+		case 0x20: {
+			const pid_20* val = reinterpret_cast<const pid_20*>(d);
+
+			StandardMetrics.ms_v_mot_temp->SetValue(val->motor_temp_C());
+			break;
+		}
+
+		// Buttons and indicators
+		case 0x102: {
+			const pid_102* val = reinterpret_cast<const pid_102*>(d);
+
+			StandardMetrics.ms_v_env_on->SetValue(val->ignition);
+			StandardMetrics.ms_v_env_footbrake->SetValue(val->rear_brake ? 1.0f : 0.0f);
+			StandardMetrics.ms_v_env_handbrake->SetValue(val->front_brake);
+			StandardMetrics.ms_v_env_headlights->SetValue(val->high_beam);
+			break;
+		}
+
+		// Engine speeds
+		case 0x104: {
+			const pid_104* val = reinterpret_cast<const pid_104*>(d);
+
+			StandardMetrics.ms_v_mot_rpm->SetValue(val->rpm);
+			StandardMetrics.ms_v_pos_speed->SetValue(val->speed_kmh());
+			StandardMetrics.ms_v_pos_odometer->SetValue(val->odometer_km());
+			StandardMetrics.ms_v_env_gear->SetValue(val->reverse ? -1 : 1);
+			break;
+		}
+
+		// Buttons and indicators
+		case 0x109: {
+			const pid_109* val = reinterpret_cast<const pid_109*>(d);
+
+			StandardMetrics.ms_v_env_throttle->SetValue(val->throttle_pc());
+			break;
+		}
+
+		// Pack status
+		case 0x200: {
+			const pid_200* val = reinterpret_cast<const pid_200*>(d);
+
+			StandardMetrics.ms_v_bat_soc    ->SetValue(val->soc());
+			StandardMetrics.ms_v_bat_soh    ->SetValue(val->soh());
+			StandardMetrics.ms_v_bat_voltage->SetValue(val->pack_voltage());
+			StandardMetrics.ms_v_bat_current->SetValue(val->pack_current());
+			StandardMetrics.ms_v_bat_power  ->SetValue(val->power_W() * 0.001f);
+			StandardMetrics.ms_v_bat_temp   ->SetValue(val->temp_cell_max());
+
+			StandardMetrics.ms_v_bat_pack_tmin->SetValue(val->temp_cell_min());
+			StandardMetrics.ms_v_bat_pack_tmax->SetValue(val->temp_cell_max());
+			break;
+		}
+
+		// Pack cells
+		case 0x203: {
+			const pid_203* val = reinterpret_cast<const pid_203*>(d);
+
+			StandardMetrics.ms_v_bat_pack_vmin->SetValue(val->min_cell_voltage() * 0.001f); // mV to V
+			StandardMetrics.ms_v_bat_pack_vmax->SetValue(val->max_cell_voltage() * 0.001f);
+			StandardMetrics.ms_v_bat_pack_vavg->SetValue(val->avg_cell_voltage() * 0.001f);
+			break;
+		}
+
+		// GPS
+		case 0x410: {
+			uint16_t msg = *reinterpret_cast<uint16_t*>(d);
+			switch (msg) {
+				case msg_gps_latspeed: {
+					const pid_410_gps_latspdcourse* val = reinterpret_cast<const pid_410_gps_latspdcourse*>(d);
+
+					StandardMetrics.ms_v_pos_latitude->SetValue(val->latitude());
+					StandardMetrics.ms_v_pos_direction->SetValue(val->course());
+					StandardMetrics.ms_v_pos_gpsspeed->SetValue(val->speed());
+					break;
+				}
+
+				case msg_gps_longalt: {
+					const pid_410_gps_longalt* val = reinterpret_cast<const pid_410_gps_longalt*>(d);
+
+					StandardMetrics.ms_v_pos_longitude->SetValue(val->longitude());
+					StandardMetrics.ms_v_pos_altitude->SetValue(val->altitude());
+					StandardMetrics.ms_v_pos_gpslock->SetValue(val->fix() != 0);
+					break;
+				}
+
+				case msg_gps_datetime: {
+					const pid_410_gps_date* val = reinterpret_cast<const pid_410_gps_date*>(d);
+
+					StandardMetrics.ms_v_pos_gpstime->SetValue(val->time_utc()); // FIXME: do not convert for each message: keep last one and convert only when required (by IncomingPollReply()?)
+					StandardMetrics.ms_v_pos_satcount->SetValue(val->n_satellites());
+					break;
+				}
+			}
+
+			break;
+		}
+	}
+
+}
+
+void OvmsVehicleEnergica::Ticker1(uint32_t ticker)
+  {
+  }
+
+void OvmsVehicleEnergica::Ticker60(uint32_t ticker)
+  {
+	// TODO: Display charge statistics, etc. (when relevant)
+  if (StandardMetrics.ms_v_charge_inprogress->AsBool())
+	{
+	}
+  else
+	{
+	// Vehicle is not charging
+	}
+  }
---
+++ vehicle/OVMS.V3/components/vehicle_energica/src/vehicle_energica_pids.h
@@ -0,0 +1,202 @@
+#ifndef VEHICLE_ENERGICA_PIDS_H
+#define VEHICLE_ENERGICA_PIDS_H
+
+#include <stdint.h>
+#include <arpa/inet.h>
+// #include <endian.h> // FIXME: doesn't work, but ESP32 is little-endian so we can keep words as-is
+#include <ctime>
+
+#pragma pack(1)
+class pid_20 { // little-endian
+
+public:
+	// IGBT in 0.1° C
+	uint16_t igbt_min;
+	uint16_t igbt;
+	uint16_t igbt_max;
+
+	uint16_t motor_temp; // 0.1° C
+
+	float motor_temp_C() const { return motor_temp * 0.1f; }
+};
+
+#pragma pack(1)
+class pid_102 { // little-endian
+
+public:
+	// Byte 0
+	bool mode_left  : 1;
+	bool mode_right : 1;
+	bool mode_push  : 1;
+	bool blink_right : 1;
+	bool blink_left  : 1;
+	bool blink_push	 : 1;
+	bool high_beam : 1;
+	bool low_beam  : 1;
+
+	// Byte 1
+	bool _unk0 : 1;
+	bool charging : 1;
+	bool _unk1 : 2; // Mode?
+	bool key_in : 1;
+	bool stand_up : 1; // 0 when stand is down
+	bool ignition : 1;
+	bool throttle_on : 1; // 1 when throttle is not idle (>0%), 0 when 0% throttle
+
+	// Byte 2
+	bool plugged : 2;
+	bool charge_port_unlocked : 1;
+	bool _unk2 : 2;
+	bool front_brake : 1;
+	bool rear_brake : 1;
+	bool _unk3 : 1;
+
+	int16_t lateral_acceleration; // g
+	int16_t frontal_acceleration; // g
+
+	uint8_t _unk4; // Remainder
+};
+
+#pragma pack(1)
+class pid_104 { // little-endian
+
+public:
+	uint32_t odometer : 32; // 0.1 km
+	uint16_t speed    : 13; // 0.1 km/h
+	uint16_t rpm      : 15;
+	uint8_t _unk0     : 3;
+	bool reverse      : 1;
+
+	float odometer_km() const { return odometer * 0.1f; }
+	float speed_kmh  () const { return speed * 0.1f; }
+};
+
+#pragma pack(1)
+class pid_109 { // little-endian
+	uint16_t _throttle; // 0.1%
+	uint16_t _current_max_out; // 0.1A
+	uint16_t _current_max_regen; // 0.1A
+	uint16_t _unk0; // Some other current
+
+public:
+	float throttle_pc()       const { return _throttle * 0.1f; }
+	float current_max_out()   const { return _current_max_out * 0.1f; }
+	float current_max_regen() const { return _current_max_regen * 0.1f; }
+};
+
+#pragma pack(1)
+class pid_200 { // big-endian
+	uint8_t _temp_cell_min;
+	uint8_t _soc;
+	uint8_t _soh;
+	uint8_t _temp_cell_max;
+	uint16_t _pack_voltage;
+	int16_t _pack_current;
+
+public: // Conversion from big-endian
+	uint8_t temp_cell_min() const { return _temp_cell_min; }
+	uint8_t soc()           const { return _soc; }
+	uint8_t soh()           const { return _soh; }
+	uint8_t temp_cell_max() const { return _temp_cell_max; }
+	uint16_t pack_voltage() const { return ::ntohs(_pack_voltage); }
+	uint16_t pack_current() const { return ::ntohs(_pack_current); }
+	uint32_t power_W()      const { return pack_voltage() * pack_current(); }
+};
+
+#pragma pack(1)
+class pid_203 { // big-endian
+	uint16_t _avg_cell_voltage; // mV
+	uint8_t  _min_cell_num;
+	uint8_t  _max_cell_num;
+	uint16_t _min_cell_voltage; // mV
+	uint16_t _max_cell_voltage; // mV
+
+public: // Conversion from big-endian
+	uint16_t avg_cell_voltage() const { return ::ntohs(_avg_cell_voltage); }
+	uint8_t  min_cell_num    () const { return _min_cell_num; }
+	uint8_t  max_cell_num    () const { return _max_cell_num; }
+	uint16_t min_cell_voltage() const { return ::ntohs(_min_cell_voltage); }
+	uint16_t max_cell_voltage() const { return ::ntohs(_max_cell_voltage); }
+};
+
+constexpr uint16_t msg_gps_latspeed  = 0x1a00;
+constexpr uint16_t msg_gps_longalt   = 0x1a01;
+constexpr uint16_t msg_gps_datetime  = 0x1afe;
+
+#pragma pack(1)
+class pid_410_gps_latspdcourse {
+	uint16_t _msg : 16; // 0x1a00
+
+	uint16_t _course : 9; // ° mag
+	uint16_t _speed  : 9; // km/h
+
+	uint16_t _lat_min_10000 : 14;
+	uint8_t _lat_min        : 6;
+	uint8_t _lat_degree     : 8;
+	bool    _lat_neg        : 1;
+
+	bool _unk1 : 1;
+
+public:
+	float course() const { return _course; }
+	float speed()  const { return _speed; }
+	float latitude() const {
+		float lat = (_lat_min_10000 /  600000.0f) + (_lat_min / 60.0f) + _lat_degree;
+		return (_lat_neg ? -lat : lat);
+	}
+};
+#pragma pack(1)
+class pid_410_gps_longalt {
+	uint16_t _msg : 16; // 0x1a01
+
+	uint8_t  _fix  : 2;
+
+	uint16_t _altitude : 15;
+	bool     _alt_neg  : 1;
+
+	uint16_t _long_min_10000 : 14;
+	uint8_t _long_min        : 6;
+	uint8_t _long_degree     : 8;
+	bool    _long_neg        : 1;
+
+	bool _unk1 : 1;
+
+public:
+	float fix() const { return _fix; }
+	float altitude() const { return (_alt_neg ? _altitude : -_altitude); }
+	float longitude() const {
+		float lon = (_long_min_10000 /  600000.0f) + (_long_min / 60.0f) + _long_degree;
+		return (_long_neg ? -lon : lon);
+	}
+};
+#pragma pack(1)
+class pid_410_gps_date {
+	uint16_t _msg : 16; // 0x1afe
+
+	uint16_t _ms    : 10;
+	uint8_t  _sec   : 6;
+	uint8_t  _min   : 6;
+	uint8_t  _hours : 5;
+
+	uint8_t  _day   : 5;
+	uint8_t  _month : 4;
+	uint8_t  _year  : 7;
+
+	uint8_t  _nsat	: 5;
+
+public:
+	float n_satellites() const { return _nsat; }
+	int64_t time_utc() const {
+		std::tm tm{};
+		tm.tm_sec = _sec;
+		tm.tm_min = _min;
+		tm.tm_hour = _hours;
+		tm.tm_mday = _day;
+		tm.tm_mon = _month;
+		tm.tm_year = _year + 100;
+		std::time_t t = std::mktime(&tm);
+		return static_cast<int64_t>(std::mktime(std::gmtime(&t)));
+	}
+};
+
+#endif // VEHICLE_ENERGICA_PIDS_H
