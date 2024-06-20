4d54f4c43aa9e6f4c086e80d7cf3f0a1f8d596de
Matthieu Labas <matthieu.labas@deepgray.fr>
2024-06-20 15:03:16 +0200

    Energica - GPS message ID is big-endian but rest of the message is little-endian.

--- vehicle/OVMS.V3/components/vehicle_energica/src/vehicle_energica_pids.h
+++ vehicle/OVMS.V3/components/vehicle_energica/src/vehicle_energica_pids.h
@@ -6,7 +6,7 @@
 // #include <endian.h> // FIXME: doesn't work, but ESP32 is little-endian so we can keep words as-is
 #include <ctime>
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_20 { // little-endian
 
 public:
@@ -19,9 +19,10 @@ public:
 
 	float motor_temp_C() const { return motor_temp * 0.1f; }
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_20) == 8, "Energica: sizeof pid_20");
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_102 { // little-endian
 
 public:
@@ -57,9 +58,10 @@ public:
 
 	uint8_t _unk4; // Remainder
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_102) == 8, "Energica: sizeof pid_102");
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_104 { // little-endian
 
 public:
@@ -72,9 +74,10 @@ public:
 	float odometer_km() const { return odometer * 0.1f; }
 	float speed_kmh  () const { return speed * 0.1f; }
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_104) == 8, "Energica: sizeof pid_104");
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_109 { // little-endian
 	uint16_t _throttle; // 0.1%
 	uint16_t _current_max_out; // 0.1A
@@ -86,9 +89,10 @@ public:
 	float current_max_out()   const { return _current_max_out * 0.1f; }
 	float current_max_regen() const { return _current_max_regen * 0.1f; }
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_109) == 8, "Energica: sizeof pid_109");
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_200 { // big-endian
 	uint8_t _temp_cell_min;
 	uint8_t _soc;
@@ -102,13 +106,14 @@ public: // Conversion from big-endian
 	uint8_t soc()           const { return _soc; }
 	uint8_t soh()           const { return _soh; }
 	uint8_t temp_cell_max() const { return _temp_cell_max; }
-	uint16_t pack_voltage() const { return ::ntohs(_pack_voltage); }
-	uint16_t pack_current() const { return ::ntohs(_pack_current); }
-	uint32_t power_W()      const { return pack_voltage() * pack_current(); }
+	float   pack_voltage()  const { return ::ntohs(_pack_voltage) * 0.1f; }
+	float   pack_current()  const { return ((int16_t)::ntohs(_pack_current)) * 0.1f; }
+	float   power_W()       const { return pack_voltage() * pack_current(); }
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_200) == 8, "Energica: sizeof pid_200");
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_203 { // big-endian
 	uint16_t _avg_cell_voltage; // mV
 	uint8_t  _min_cell_num;
@@ -123,12 +128,13 @@ public: // Conversion from big-endian
 	uint16_t min_cell_voltage() const { return ::ntohs(_min_cell_voltage); }
 	uint16_t max_cell_voltage() const { return ::ntohs(_max_cell_voltage); }
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_203) == 8, "Energica: sizeof pid_203");
 
 
-constexpr uint16_t msg_gps_latspeed  = 0x1a00;
-constexpr uint16_t msg_gps_longalt   = 0x1a01;
-constexpr uint16_t msg_gps_datetime  = 0x1afe;
+constexpr uint16_t msg_gps_latspeed  = 0x001a;
+constexpr uint16_t msg_gps_longalt   = 0x011a;
+constexpr uint16_t msg_gps_datetime  = 0xfe1a;
 
 /**
  * All 0x410 PIDs start with a 2-bytes marker identifying the rest of the data (message type).
@@ -136,13 +142,17 @@ constexpr uint16_t msg_gps_datetime  = 0x1afe;
  * define their memory mapping.
  */
 
-#pragma pack(1)
-struct _pid_410 {
+#pragma pack(push,1)
+class _pid_410 {
 	uint16_t _msg : 16;
+
+public:
+	uint16_t msg() const { return _msg; }
 };
+#pragma pack(pop)
 static_assert(sizeof(_pid_410) == 2, "Energica: sizeof _pid_410");
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_410_gps_latspdcourse : _pid_410 {
 	uint16_t _course : 9; // ° mag
 	uint16_t _speed  : 9; // km/h
@@ -162,11 +172,23 @@ public:
 		return (_lat_neg ? -lat : lat);
 	}
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_410_gps_latspdcourse) == 8, "Energica: sizeof pid_410 (latitude)");
 
-#pragma pack(1)
+// See https://support.dewesoft.com/en/support/solutions/articles/14000108531-explanation-of-gps-fix-quality-values
+enum gps_fix : uint8_t {
+	not_fixed = 0,
+	standalone, // Only from GNSS satellites
+	dgps,       // Differential GPS -> beside GNSS satellites an additional correction from base station is used. Sometimes this status is shown if the received uses additional SBAS signal for the position
+	gps_pps,
+	// Those values are beyond the 2-bits representation but given for educational purposes
+	// rtk_fixed,
+	// rtk_float,
+};
+
+#pragma pack(push,1)
 class pid_410_gps_longalt : _pid_410 {
-	uint8_t  _fix  : 2;
+	gps_fix  _fix  : 2;
 
 	uint16_t _altitude : 15;
 	bool     _alt_neg  : 1;
@@ -179,16 +201,17 @@ class pid_410_gps_longalt : _pid_410 {
 	bool _unk1 : 1;
 
 public:
-	float fix      () const { return _fix; }
-	float altitude () const { return (_alt_neg ? -_altitude : _altitude); }
-	float longitude() const {
+	gps_fix fix      () const { return _fix; }
+	float   altitude () const { return (_alt_neg ? -_altitude : _altitude); }
+	float   longitude() const {
 		float lon = (_long_min_10000 /  600000.0f) + (_long_min / 60.0f) + _long_degree;
 		return (_long_neg ? -lon : lon);
 	}
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_410_gps_longalt) == 8, "Energica: sizeof pid_410 (longitude)");
 
-#pragma pack(1)
+#pragma pack(push,1)
 class pid_410_gps_date : _pid_410 {
 	uint16_t _ms    : 10;
 	uint8_t  _sec   : 6;
@@ -209,7 +232,7 @@ public:
 	uint8_t month  () const { return _month; }
 	int     year   () const { return _year + 2000; }
 
-	float n_satellites() const { return _nsat; }
+	uint8_t n_satellites() const { return _nsat; }
 	int64_t time_utc() const {
 		std::tm tm{};
 		tm.tm_sec  = _sec;
@@ -221,6 +244,7 @@ public:
 		return static_cast<int64_t>(std::mktime(&tm)); // GPS time is already in UTC // TODO: is it since epoch?
 	}
 };
+#pragma pack(pop)
 static_assert(sizeof(pid_410_gps_date) == 8, "Energica: sizeof pid_410 (date)");
 
 #endif // VEHICLE_ENERGICA_PIDS_H
