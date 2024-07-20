openvehicles/Open-Vehicle-Monitoring-System-3

commit 2915281af0715a46de548f80a0104d4d9492e400

Poller - int32_t format strings changes
In ESP-IDF v5+, a compiler change has been made for type int32_t and uint32_t : Cf https://docs.espressif.com/projects/esp-idf/en/latest/esp32/migration-guides/release-5.x/5.0/gcc.html#int32-t-and-uint32-t-for-xtensa-compiler

This change forces us to update the `printf. format strings and use, as much as possible, the [PRI... portable format constants](https://en.cppreference.com/w/cpp/types/integer)

In addition, a constant in `Atomic_Increment()` call needed to change in order to compile with ESP-IDF v5+ compiler.

vehicle/OVMS.V3/components/poller/src/vehicle_poller.cpp
@@ -292,7 +292,7 @@ void OvmsPoller::PollerNextTick(poller_source_t source)
   {
   // Completed checking all poll entries for the current m_poll.ticker
 
-  IFTRACE(Poller) ESP_LOGD(TAG, "[%" PRIu8 "]PollerNextTick(%s): cycle complete for ticker=%u", m_poll.bus_no, PollerSource(source), m_poll.ticker);
+  IFTRACE(Poller) ESP_LOGD(TAG, "[%" PRIu8 "]PollerNextTick(%s): cycle complete for ticker=%" PRIu32, m_poll.bus_no, PollerSource(source), m_poll.ticker);
   m_poll_ticked = false;
 
   if (source == poller_source_t::Primary)
@@ -466,7 +466,7 @@ void OvmsPoller::PollerSend(poller_source_t source)
       break;
     case OvmsNextPollResult::FoundEntry:
       {
-      ESP_LOGD(TAG, "[%" PRIu8 "]PollerSend(%s)[%" PRIu8 "]: entry at[type=%02X, pid=%X], ticker=%u, wait=%u, cnt=%u/%u",
+      ESP_LOGD(TAG, "[%" PRIu8 "]PollerSend(%s)[%" PRIu8 "]: entry at[type=%02X, pid=%X], ticker=%" PRIu32 ", wait=%u, cnt=%u/%u",
              m_poll.bus_no, PollerSource(source), m_poll_state, m_poll.entry.type, m_poll.entry.pid,
              m_poll.ticker, m_poll_wait, m_poll_sequence_cnt, m_poll_sequence_max);
       // We need to poll this one...
@@ -1713,7 +1713,7 @@ void OvmsPollers::Queue_PollerFrame(const CAN_frame_t &frame, bool success, bool
   if (xQueueSend(m_pollqueue, &entry, 0) != pdPASS)
     {
     volatile uint32_t &count = m_overflow_count[istx ? 1 : 0];
-    Atomic_Increment(count, 1U);
+    Atomic_Increment(count, 1lU);
     }
   }
 
@@ -1786,7 +1786,7 @@ void OvmsPollers::PollerStatus(int verbosity, OvmsWriter* writer)
     if (last == 0)
       writer->puts("None");
     else
-      writer->printf("%" PRIu8 "s (ticks)\n", (curmon - last));
+      writer->printf("%" PRIu32 "s (ticks)\n", (curmon - last));
     }
   if (!found_list)
     {
@@ -2085,11 +2085,11 @@ bool OvmsPollers::LoadTimesTrace( metric_unit_t ratio_unit, times_trace_t &trace
     switch (it->first.entry_type)
       {
       case OvmsPoller::OvmsPollEntryType::FrameRx:
-        item.desc = string_format("RxCan%" PRIu32 "[%03" PRIx32 "]",
+        item.desc = string_format("RxCan%" PRIu8 "[%03" PRIx32 "]",
             it->first.busnumber, it->first.Frame_MsgId);
         break;
       case OvmsPoller::OvmsPollEntryType::FrameTx:
-        item.desc = string_format("TxCan%" PRIu32 "[%03" PRIx32 "]",
+        item.desc = string_format("TxCan%" PRIu8 "[%03" PRIx32 "]",
             it->first.busnumber, it->first.Frame_MsgId);
         break;
       case OvmsPoller::OvmsPollEntryType::Poll:

vehicle/OVMS.V3/components/poller/src/vehicle_poller_isotp.cpp
@@ -57,7 +57,7 @@ void OvmsPoller::PollerISOTPStart(bool fromTicker)
     m_poll.moduleid_high = 0x7ef;
     }
 
-  ESP_LOGD(TAG, "[%" PRIu8 "]PollerISOTPStart(%s): send [bus=%" PRIu8 ", type=%02" PRIX16 ", pid=%X], expecting %03x/%03x-%03x",
+  ESP_LOGD(TAG, "[%" PRIu8 "]PollerISOTPStart(%s): send [bus=%" PRIu8 ", type=%02" PRIX16 ", pid=%X], expecting %03" PRIx32 "/%03" PRIx32 "-%03" PRIx32 "",
            m_poll.bus_no, fromTicker ? "Yes" : "No",
            m_poll.entry.pollbus, m_poll.type, m_poll.pid, m_poll.moduleid_sent,
            m_poll.moduleid_low, m_poll.moduleid_high);

commit f801bc9fc9f368e27209426d0ec390953b45279d

fix compilation error
Fix compilation error introduced in 2915281af0715a46de548f80a0104d4d9492e400 / #1032

vehicle/OVMS.V3/components/poller/src/vehicle_poller.cpp
@@ -1713,7 +1713,7 @@ void OvmsPollers::Queue_PollerFrame(const CAN_frame_t &frame, bool success, bool
   if (xQueueSend(m_pollqueue, &entry, 0) != pdPASS)
     {
     volatile uint32_t &count = m_overflow_count[istx ? 1 : 0];
-    Atomic_Increment(count, 1lU);
+    Atomic_Increment(count, (uint32_t)1);
     }
   }
 