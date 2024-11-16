c277b9daa2d295e6c9a9334b30a99aac7445f427
Michael Balzer <balzer@expeedo.de>
2024-11-16 15:06:52 +0100

    Server V2: fix generator ("X") record field separators

--- vehicle/OVMS.V3/components/ovms_server_v2/src/ovms_server_v2.cpp
+++ vehicle/OVMS.V3/components/ovms_server_v2/src/ovms_server_v2.cpp
@@ -1176,7 +1176,7 @@ void OvmsServerV2::TransmitMsgGen(bool always)
     << std::setprecision(1)
     << "MP-0 X"
     << StandardMetrics.ms_v_gen_inprogress->AsBool()
-    << ";"
+    << ","
     << StandardMetrics.ms_v_gen_pilot->AsBool()
     << ","
     << StandardMetrics.ms_v_gen_voltage->AsInt()
@@ -1218,7 +1218,7 @@ void OvmsServerV2::TransmitMsgGen(bool always)
     << StandardMetrics.ms_v_gen_duration_range->AsInt()
     << ","
     << StandardMetrics.ms_v_gen_duration_soc->AsInt()
-    << ";"
+    << ","
     << StandardMetrics.ms_v_gen_temp->AsFloat()
     << ","
     << StandardMetrics.ms_v_gen_timestamp->AsString("-1", Seconds, 0)
