0ce5299e32cebf32fe877dfca76696a355973426
Peter Harry <prharry@hotmail.com>
2025-04-21 11:43:36 +1000

    Revert "Update vehicle_mg5.cpp"
    This reverts commit 8d4160c67b2fa57cd1fa8d65d83a0d61e789a4c9.

--- vehicle/OVMS.V3/components/vehicle_mgev/src/vehicle_mg5.cpp
+++ vehicle/OVMS.V3/components/vehicle_mgev/src/vehicle_mg5.cpp
@@ -161,10 +161,8 @@ OvmsVehicleMg5::OvmsVehicleMg5()
              m_max_dc_charge_rate->AsFloat());
         
     //Add variant specific poll data
-    //ConfigureMG5PollData(mg5_obdii_polls, sizeof(mg5_obdii_polls));
-    RegisterCanBus(1,CAN_MODE_ACTIVE,CAN_SPEED_500KBPS, nullptr, false);
-    PollSetState(PollStateListenOnly);
-    PollSetPidList(m_can1,mg5_obdii_polls);
+    ConfigureMG5PollData(mg5_obdii_polls, sizeof(mg5_obdii_polls));
+    
     //BMS Configuration
     BmsSetCellArrangementVoltage(42, 2);
     BmsSetCellArrangementTemperature(42, 2);
