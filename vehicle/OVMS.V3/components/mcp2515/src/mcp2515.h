0fd0daee30fb415c0d9420f0e52eeac0c339ec48
zbchristian <christian@zeitnitz.eu>
2025-07-11 19:09:51 +0200

    Set CAN transceiver active/listen-only mode according to CAN bus mode

--- vehicle/OVMS.V3/components/mcp2515/src/mcp2515.h
+++ vehicle/OVMS.V3/components/mcp2515/src/mcp2515.h
@@ -94,6 +94,7 @@ class mcp2515 : public canbus
   public:
     void SetPowerMode(PowerMode powermode);
     bool GetErrorFlagsDesc(std::string &buffer, uint32_t error_flags) override;
+    void SetTransceiverMode(bool isactive);
 
   public:
     static void shell_setaccfilter(int verbosity, OvmsWriter* writer, OvmsCommand* cmd, int argc, const char* const* argv);
