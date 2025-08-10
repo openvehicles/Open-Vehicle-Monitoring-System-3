48f09e6daf5819401ba60dea13725a8a45a52df4
Michael Balzer <balzer@expeedo.de>
2025-08-10 09:02:10 +0200

    TLS: add DigiCert Global Root G2 certificate (fixes Pushover SSL error)

--- vehicle/OVMS.V3/components/ovms_tls/component.mk
+++ vehicle/OVMS.V3/components/ovms_tls/component.mk
@@ -11,5 +11,5 @@ ifdef CONFIG_MG_ENABLE_SSL
 COMPONENT_SRCDIRS := src
 COMPONENT_ADD_INCLUDEDIRS := src
 COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
-COMPONENT_EMBED_FILES := trustedca/usertrust.crt trustedca/digicert_global.crt trustedca/starfield_class2.crt trustedca/baltimore_cybertrust.crt trustedca/isrg_x1.crt
+COMPONENT_EMBED_FILES := trustedca/usertrust.crt trustedca/digicert_global.crt trustedca/digicert_g2.crt trustedca/starfield_class2.crt trustedca/baltimore_cybertrust.crt trustedca/isrg_x1.crt
 endif
