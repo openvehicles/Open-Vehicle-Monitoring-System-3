1a1402cb2af202b6189aebae94054ccbcf7eb4f8
zorgms <100448801+zorgms@users.noreply.github.com>
2025-09-05 17:50:27 +0200

    update 2

--- vehicle/OVMS.V3/components/ovms_cellular/src/ovms_cellular.cpp
+++ vehicle/OVMS.V3/components/ovms_cellular/src/ovms_cellular.cpp
@@ -668,6 +668,7 @@ void modem::State1Enter(modem_state1_t newstate)
 
     case NetWait:
       MyEvents.SignalEvent("system.modem.netwait", NULL);
+      if (m_ppp != NULL && !m_ppp->m_connected)
         {
         // Guard against calling muxtx before MUX POLL channel is actually open (was causing crash):
         if (m_mux && m_mux->IsChannelOpen(m_mux_channel_POLL))
