63eb7ad87ede0a68866e4b2710dd65126cb23ff1
zorgms <100448801+zorgms@users.noreply.github.com>
2025-12-13 14:47:34 +0100

    Update eq_can.cpp

--- vehicle/OVMS.V3/components/vehicle_smarteq/src/eq_can.cpp
+++ vehicle/OVMS.V3/components/vehicle_smarteq/src/eq_can.cpp
@@ -98,9 +98,13 @@ void OvmsVehicleSmartEQ::IncomingFrameCan1(CAN_frame_t* p_frame) {
         }
       break;
     case 0x392:
+      {
       REQ_DLC(6);
+      bool cabin_plausible = (float)(CAN_BYTE(5) - 40.0f) != 0.0f;
       StdMetrics.ms_v_env_hvac->SetValue((CAN_BYTE(1) & 0x40) > 0);
-      StdMetrics.ms_v_env_cabintemp->SetValue(CAN_BYTE(5) - 40.0f);
+      if (cabin_plausible)
+        StdMetrics.ms_v_env_cabintemp->SetValue((float)(CAN_BYTE(5) - 40.0f));
+      }
       break;
     case 0x42E:        // HV voltage / temp frame
       {
@@ -301,7 +305,7 @@ void OvmsVehicleSmartEQ::IncomingFrameCan1(CAN_frame_t* p_frame) {
       // TPMS pressure values only used, when CAN write is disabled, otherwise utilize PollReply_TPMS_InputCapt
       if (!m_enable_write)
       {
-        REQ_DLC(7);
+        REQ_DLC(6);
         // Read TPMS pressure values:
         for (int i = 0; i < 4; i++) 
           {
