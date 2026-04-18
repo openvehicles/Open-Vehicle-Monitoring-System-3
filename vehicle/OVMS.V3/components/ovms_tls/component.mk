c8159b2444ffee023ae986eb92c0fd02e55cc00d
shadowguardian507-irl <shadow@shadowguardian507-irl.uk>
2026-04-18 15:51:50 +0100

    atomic fix - typo correction

--- vehicle/OVMS.V3/components/ovms_tls/component.mk
+++ vehicle/OVMS.V3/components/ovms_tls/component.mk
@@ -12,3 +12,4 @@ COMPONENT_SRCDIRS := src
 COMPONENT_ADD_INCLUDEDIRS := src
 COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
 COMPONENT_EMBED_FILES := trustedca/usertrust.crt trustedca/digicert_global.crt trustedca/digicert_g2.crt trustedca/starfield_class2.crt trustedca/baltimore_cybertrust.crt trustedca/isrg_x1.crt
+endif
