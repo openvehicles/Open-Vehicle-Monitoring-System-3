c8ec1d460c957868f941a5e5839e84cdffc23e0d
zorgms <100448801+zorgms@users.noreply.github.com>
2026-05-08 16:41:36 +0200

    updated vehicle.cpp too

--- vehicle/OVMS.V3/components/vehicle_nissanleaf/src/vehicle_nissanleaf.cpp
+++ vehicle/OVMS.V3/components/vehicle_nissanleaf/src/vehicle_nissanleaf.cpp
@@ -2655,6 +2655,7 @@ OvmsVehicle::vehicle_command_t OvmsVehicleNissanLeaf::CommandClimateControl(bool
   ESP_LOGI(TAG, "CommandClimateControl");
   if (!climatecontrolon)
     {
+    // stops the scheduled climate restart
     m_climate_restart = false;
     m_climate_restart_ticker = 0;
     }
